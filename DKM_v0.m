3800d8673269098b448b831e54765025fbf69e70
aety <ay@u.northwestern.edu>
2018-09-10 18:19:30 +0200

    define helix by spatial period instead of frequency

--- DKM_v0.m
+++ DKM_v0.m
@@ -4,7 +4,7 @@ L_res = 0.1; % catheter node points resolution (mm)
 
 pct_mark = 20; % length of catheter with marker (mm)
 amp_mark = 1; % amplitude of the helix (mm)
-fre_mark = 0.5; % spatial frequency of the helix (count)
+per_mark = 2; % spatial period of the helix (mm)
 
 %% calculate geometry
 X = 0:L_res:L; % catheter X
@@ -12,7 +12,7 @@ Y = zeros(length(X),1); % catheter Y
 
 len_mark = 0.01*pct_mark*L;
 x_mark = 0:L_res:len_mark; 
-y_mark = amp_mark*sin(2*pi*fre_mark*x_mark); % helix Y);
+y_mark = amp_mark*sin(2*pi*x_mark/per_mark);
 x_mark = x_mark + (L - len_mark); % helix X
 
 %% visualize
