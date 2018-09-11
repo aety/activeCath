e78f9fb322d41baead618fc61328aee957187d1c
aety <ay@u.northwestern.edu>
2018-09-11 17:53:46 +0200

    finished DKM_v1.m, but it is problematic with bending starting partially through the length.

--- DKM_v1.m
+++ DKM_v1.m
@@ -1,12 +1,13 @@
 %% define parameters
-L = 50; % catheter length (mm)
+L = 100; % catheter length (mm)
 L_res = 1; % catheter node points resolution (mm)
 
-pct_mark = 20; % length of catheter with marker (mm)
-amp_mark = 1; % amplitude of the helix (mm)
-per_mark = 2; % spatial period of the helix (mm)
+% % pct_mark = 20; % length of catheter with marker (mm)
+% % amp_mark = 1; % amplitude of the helix (mm)
+% % per_mark = 2; % spatial period of the helix (mm)
 
-th_end = 30; % rotation of each link w.r.t. previous (deg)
+pct_bend = 100; % percent of catheter length to bend (%)
+th_bend = 30; % rotation of each link w.r.t. previous (deg)
 %% calculate geometry
 
 X = 0:L_res:L; % catheter X
@@ -14,10 +15,15 @@ X = 0:L_res:L; % catheter X
 n = nan(3,length(X));
 n(:,1) = [0;0;1];
 
-th = th_end/length(X);
+npt_org = round(0.01*(100 - pct_bend)*length(X)); % number of nodes unbent (node index from which bending begins)
+npt_bend = (length(X) - npt_org); % number of nodes bent
 
+th = th_bend/npt_bend; % bending angle for each segment (evenly divided)
+th_arr = [zeros(1,npt_org),th*ones(1,npt_bend)]; % full array of bending angles for each segment)
+
+hold on;
 for ii = 2:length(X)
-    n(:,ii) = M_rotate(th*pi/180)*M_transl(L_res)*n(:,ii-1); 
+    n(:,ii) = M_rotate(th_arr(ii)*pi/180)*M_transl(L_res)*n(:,ii-1); 
 end
 
 X = n(1,:);
@@ -30,7 +36,9 @@ Y = n(2,:);
 % x_mark = x_mark + (L - len_mark); % helix X
 
 %% visualize
+figure(1);
 plot(X,Y,'.-');
 hold on;
 % plot(x_mark,y_mark,'.-');
 axis equal
+set(gcf,'position',[-1919, -226, 1920, 963]);
