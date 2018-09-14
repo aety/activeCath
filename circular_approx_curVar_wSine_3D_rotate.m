4daa170f3f5dda8a43dacc3b947f34ebddd07383
aety <ay@u.northwestern.edu>
2018-09-14 16:57:48 +0200

    modified the 3D rotation video

--- circular_approx_curVar_wSine_3D_rotate.m
+++ circular_approx_curVar_wSine_3D_rotate.m
@@ -5,15 +5,15 @@ vidflag = 1;
 
 if vidflag
     anim = VideoWriter(datestr(datetime('now'),'yyyy-mm-dd-HHMMss'),'Motion JPEG AVI');
-    anim.FrameRate = 1;
+    anim.FrameRate = 3;
     open(anim);
 end
 
 %% define catheter 3D rotation (about x-axis)
-rot_arr = 0:10:90; % array of angles to rotate the catheter by (deg)
+rot_arr = [0:10:90,80:-10:0]; % array of angles to rotate the catheter by (deg)
 
 %% define helix
-p1_helix = 80;      % helix starting point (% length)
+p1_helix = 70;      % helix starting point (% length)
 p2_helix = 95;      % helix ending point (% length)
 npt_helix = 100;    % number of points of the helix
 a_helix = 1;        % amplitude of the sine wave of the helix (mm)
@@ -25,10 +25,10 @@ res = 0.5;      % catheter spatial resolution (interval between nodes) (mm)
 pct_bent = 70;  % percent length bent (%)
 
 %% define varying parameter and associated file name and descriptions
-variable_arr = 50:50:500; % array of values for the varying parameter
+variable_arr = 10:10:90; % array of values for the varying parameter
 
 fname = 'curVar';
-var_name = 'r_K, radius of curvature (mm)';
+var_name = '\theta_{end} (\circ)';
 con_name = ['L_{bend} = ' num2str(pct_bent) '%'];
 
 %% looop for rotation
@@ -39,16 +39,16 @@ for aa = 1:length(rot_arr)% aa = 1;
     
     %% loop for bending the catheter
     color_arr = colormap(parula(length(variable_arr)));
-    TH_END_arr = nan(1,length(variable_arr));
     
     for rr = 1:length(variable_arr)
         
-        Rk = variable_arr(rr);  % radius of curvature (to define bent shape)
+        th_end = variable_arr(rr)*pi/180;  % radius of curvature (to define bent shape)
         
         %% configure catheter
-        
         L2 = 0.01*pct_bent*L;
         L1 = L - L2;
+        
+        Rk = L2/th_end; % radius of curvature of the bent section
         
         x1 = 0:res:L1;              % x of unbent
         y1 = zeros(1,length(x1));   % y of unbent
@@ -56,16 +56,12 @@ for aa = 1:length(rot_arr)% aa = 1;
         xc = L1;    % x-location of the center of virtual circle
         yc = Rk;    % y-location of the center of virtual circle
         
-        th_c = L2/Rk;                   % total angle that the arc spans (rad)
+        th_c = th_end;                   % total angle that the arc spans (rad)
         th_incre = th_c/(L2/res);       % angle increment (rad)
         th_arr = (1:(L2/res))*th_incre; % arc angle array
         
         x2 = L1 + Rk*sin(th_arr);   % x of bent section
         y2 = Rk - Rk*cos(th_arr);   % y of bent sectoin
-        
-        th_end = atan2(y2(end)-y2(end-1),x2(end)-x2(end-1))*180/pi; % end effector theta
-        
-        TH_END_arr(rr) = th_end;
         
         X = [x1,x2];            % catheter X coordinate
         Y = [y1,y2];            % catheter Y coordinate
@@ -109,23 +105,22 @@ for aa = 1:length(rot_arr)% aa = 1;
         for pp = 1:4
             subplot(2,2,pp);
             hold on
-            h(rr) = plot3(X,Y,Z,'-','color',color_arr(rr,:)); % plot catheter
-            %     plot(X(end-1:end),Y(end-1:end),'-','linewidth',2,'color',color_arr(rr,:)); % plot end effector angle
-            %     text(X(end),Y(end),[num2str(th_end,3) '\circ'],'color',color_arr(rr,:),'fontsize',8); % label end effector angle
-            plot3(xh,yh,zh,'color',color_arr(rr,:)); % plot helix
+            plot3(X,Y,Z,'-','color',color_arr(rr,:),'linewidth',1); % plot catheter
+            plot3(xh,yh,zh,'color',color_arr(rr,:),'linewidth',1); % plot helix
         end
+        text(X(end),Y(end),Z(end),num2str(th_end*180/pi,3),'color',color_arr(rr,:));
     end
     
     %% format figure
     
-    view_arr = [-37.5+90,30; 0,90; 0,0; 90,0];
+    view_arr = [-37.5+90,30; 0,90; 90,0; 0,0];
     
     for pp = 1:4
         
         subplot(2,2,pp);
         
         axis equal;
-        xlim([0,1.2*L]);
+        xlim([0,L]);
         ylim([0,L/2]);
         zlim([0,L/2]);
         view(view_arr(pp,:));
@@ -138,27 +133,24 @@ for aa = 1:length(rot_arr)% aa = 1;
         
     end
     
-    % add information about the helix
-    subplot(2,2,1);
-    title({[con_name ', L_{helix} = ' num2str(p1_helix) '~' num2str(p2_helix) ' %'];...
-        [num2str(n_helix) ' sines at ' num2str(a_helix) ' mm']},'fontweight','normal');
+    % %     % add information about the helix
+    % %     subplot(2,2,1);
+    % %     text(0,0,L/2,{[con_name ', L_{helix} = ' num2str(p1_helix) '~' num2str(p2_helix) ' %'];...
+    % %         [num2str(n_helix) ' sines at ' num2str(a_helix) ' mm']},'fontweight','normal');
     
-    % colorbar (legend)
-    subplot(2,2,4);
-    hc = colorbar;
-    set(hc,'ytick',(1:length(variable_arr))/length(variable_arr),'yticklabel',variable_arr);
-    set(hc,'box','off');
-    ylabel(hc,var_name);
+    % % %     % colorbar (legend)
+    % % %     subplot(2,2,1);
+    % % %     hc = colorbar;
+    % % %     set(hc,'ytick',[1,length(variable_arr)]/length(variable_arr),'yticklabel',[variable_arr(1),variable_arr(end)]);
+    % % %     set(hc,'box','off');
+    % % %     ylabel(hc,var_name);
+    % % %     hc.Location = 'southoutside';
     
     % sizing and saving
-    set(gcf,'position',[520,-91,900,700]);
-    %     set(gcf,'paperposition',[0,0,4,3],'unit','inches');
-    %     print('-dtiff','-r300',['circular_approx_' fname '_wSine_3D']);
-    %     close;
+    set(gcf,'position',[100,100,600,400]);
     
     %% save frame for video
     if vidflag
-        %         vid = figure(1);
         frame = getframe(figure(1));
         writeVideo(anim,frame);
     else
@@ -176,3 +168,4 @@ end
 if vidflag
     close(anim);
 end
+close;
