6b800bded3124fcca9bf4be5e6ad868c61a990f6
aety <ay@u.northwestern.edu>
2018-09-28 13:17:59 +0200

    minor revision of the GUI catheter_7dof

--- catheter_7dof.m
+++ catheter_7dof.m
@@ -22,7 +22,7 @@ function varargout = catheter_7dof(varargin)
 
 % Edit the above text to modify the response to help catheter_7dof
 
-% Last Modified by GUIDE v2.5 27-Sep-2018 18:22:47
+% Last Modified by GUIDE v2.5 28-Sep-2018 11:56:59
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -62,22 +62,22 @@ guidata(hObject, handles);
 
 %% get default parameters
 % % % % global initialValues;
-% % % % 
+% % % %
 % % % % initialValues = [
 % % % %     str2double(get(handles.edit_L,'String'));            % length of catheter (mm)
 % % % %     str2double(get(handles.edit_L_act,'String'));   % active length (%)
 % % % %     str2double(get(handles.edit_L_res,'String'));        % number of nodes along catheter length
-% % % %     
+% % % %
 % % % %     str2double(get(handles.edit_h_1,'String'));     % helix starting point (% length)
 % % % %     str2double(get(handles.edit_h_2,'String'));     % helix ending point (% length)
 % % % %     str2double(get(handles.edit_h_n,'String'));    % number of points of the helix
 % % % %     str2double(get(handles.edit_h_a,'String'));      % amplitude of the sine wave of the helix (mm)
 % % % %     str2double(get(handles.edit_h_w,'String'));      % number of sinusoids of the helix
-% % % % 
-% % % %     str2double(get(handles.edit_x,'String')); % base x-translation 
-% % % %     str2double(get(handles.edit_y,'String')); % base y-translation 
-% % % %     str2double(get(handles.edit_z,'String')); % base z-translation 
-% % % % 
+% % % %
+% % % %     str2double(get(handles.edit_x,'String')); % base x-translation
+% % % %     str2double(get(handles.edit_y,'String')); % base y-translation
+% % % %     str2double(get(handles.edit_z,'String')); % base z-translation
+% % % %
 % % % %     get(handles.slider_roll, 'Value')*pi/180;   % roll (degree)
 % % % %     get(handles.slider_yaw, 'Value')*pi/180;     % yaw (degree)
 % % % %     get(handles.slider_pitch, 'Value')*pi/180;  % pitch (degree)
@@ -415,24 +415,24 @@ function edit17_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
-% % % 
-% % % 
-% % % 
+% % %
+% % %
+% % %
 % % % function edit18_Callback(hObject, eventdata, handles)
 % % % % hObject    handle to edit18 (see GCBO)
 % % % % eventdata  reserved - to be defined in a future version of MATLAB
 % % % % handles    structure with handles and user data (see GUIDATA)
-% % % 
+% % %
 % % % % Hints: get(hObject,'String') returns contents of edit18 as text
 % % % %        str2double(get(hObject,'String')) returns contents of edit18 as a double
-% % % 
-% % % 
+% % %
+% % %
 % % % % --- Executes during object creation, after setting all properties.
 % % % function edit18_CreateFcn(hObject, eventdata, handles)
 % % % % hObject    handle to edit18 (see GCBO)
 % % % % eventdata  reserved - to be defined in a future version of MATLAB
 % % % % handles    empty - handles not created until after all CreateFcns called
-% % % 
+% % %
 % % % % Hint: edit controls usually have a white background on Windows.
 % % % %       See ISPC and COMPUTER.
 % % % if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
@@ -585,160 +585,6 @@ end
 
 
 
-function configure_catheter_and_plot(hObject, eventdata, handles)
-%% clear axes
-cla(handles.axes1);
-cla(handles.axes2);
-cla(handles.axes3);
-
-%% define catheter
-L = str2double(get(handles.edit_L,'String'));            % length of catheter (mm)
-L_pct_bent = str2double(get(handles.edit_L_act,'String'));   % active length (%)
-L_res = str2double(get(handles.edit_L_res,'String'));        % number of nodes along catheter length
-
-%% define helix
-p1_helix = str2double(get(handles.edit_h_1,'String'));     % helix starting point (% length)
-p2_helix = str2double(get(handles.edit_h_2,'String'));     % helix ending point (% length)
-npt_helix = str2double(get(handles.edit_h_n,'String'));    % number of points of the helix
-a_helix = str2double(get(handles.edit_h_a,'String'));      % amplitude of the sine wave of the helix (mm)
-n_helix = str2double(get(handles.edit_h_w,'String'));      % number of sinusoids of the helix
-
-%% define rotation
-th_end = get(handles.slider_bend, 'Value')*pi/180;  % bending (degree)
-alpha = get(handles.slider_roll, 'Value')*pi/180;   % roll (degree)
-beta = get(handles.slider_yaw, 'Value')*pi/180;     % yaw (degree)
-gamma = get(handles.slider_pitch, 'Value')*pi/180;  % pitch (degree)
-M_rot = getRY(gamma)*getRZ(beta)*getRX(alpha);      % the associated rotation matrix
-
-%% define translation
-trans_x = str2double(get(handles.edit_x,'String')); % base x-translation 
-trans_y = str2double(get(handles.edit_y,'String')); % base y-translation 
-trans_z = str2double(get(handles.edit_z,'String')); % base z-translation 
-
-%% configure catheter
-L2 = 0.01*L_pct_bent*L;
-L1 = L - L2;
-
-Rk = L2/th_end; % radius of curvature of the bent section
-
-x1 = 0:L_res:L1;              % x of unbent
-y1 = zeros(1,length(x1));   % y of unbent
-
-xc = L1;    % x-location of the center of virtual circle
-yc = Rk;    % y-location of the center of virtual circle
-
-th_c = th_end;                   % total angle that the arc spans (rad)
-th_incre = th_c/(L2/L_res);       % angle increment (rad)
-th_angles = (1:(L2/L_res))*th_incre; % arc angle array
-
-x2 = L1 + Rk*sin(th_angles);   % x of bent section
-y2 = Rk - Rk*cos(th_angles);   % y of bent sectoin
-
-X = [x1,x2];            % catheter X coordinate
-Y = [y1,y2];            % catheter Y coordinate
-Z = zeros(1,length(X)); % catheter Z coordinate
-
-M = [X;Y;Z]; % catheter XYZ combined
-
-%% configure helix
-pct_helix = p2_helix - p1_helix; % helix global length (% of cathetler L)
-
-% define three segments along the bent length (space 1, helix, space 2) % the three variables below should sum up to 1
-frac_space_1 = (p1_helix - (100 - L_pct_bent))/L_pct_bent;  % ratio of the bent section before helix coverage (a ratio of bent length)
-frac_helix_bent = pct_helix/L_pct_bent;                   % ratio of helix coverage (a ratio of bent length)
-frac_space_2 = (100 - p2_helix)/L_pct_bent;               % ratio of the bent section after helix coverage (a ratio of bent length)
-
-% identify theta angle from the perspective of the big circle defining curvature
-th_1 = -pi/2 + frac_space_1*th_c;
-th_helix_range = frac_helix_bent*th_c;
-th_2 = th_1 + th_helix_range;
-
-% adjust the total number of sinusoids based on th_helix_range
-n_effect_helix = n_helix*2*pi/th_helix_range;           % effective number of sinusoids (adjusted for the equation)
-
-% define an array for all theta angles along the helix
-th_helix = linspace(th_2,th_1,npt_helix);
-
-% compile helix
-xh = xc + (Rk + a_helix*sin(n_effect_helix*th_helix)).*cos(th_helix);   % x location of helix
-yh = yc + (Rk + a_helix*sin(n_effect_helix*th_helix)).*sin(th_helix);   % y location of helix
-zh = a_helix*cos(n_effect_helix*th_helix);
-
-M_helix = [xh;yh;zh];
-
-%% rotate both the catheter and the helix
-M = M_rot*M;
-X = M(1,:); Y = M(2,:); Z = M(3,:);
-M_helix = M_rot*M_helix;
-xh = M_helix(1,:); yh = M_helix(2,:); zh = M_helix(3,:);
-
-
-%% translate both the catheter and the helix
-X = X + trans_x; xh = xh + trans_x;
-Y = Y + trans_y; yh = yh + trans_y;
-Z = Z + trans_z; zh = zh + trans_z;
-
-%% find apexes in X-Y projection
-[x_pks,y_pks] = func_find_apex_rot(xh,yh,X,Y,0);
-
-%% plot
-color_arr = colormap(viridis(5));
-rr = 1;
-ii_arr = 1 + [0,length(x1)];
-hh_arr = {handles.axes1,handles.axes2,handles.axes3};
-vv_arr = [-37.5,30; 0,90; 0,90];
-for hh = 1:2
-    hold(hh_arr{hh},'on');
-    plot3(X(ii_arr(hh):end),Y(ii_arr(hh):end),Z(ii_arr(hh):end),'-','color',color_arr(rr,:),'linewidth',2,'parent',hh_arr{hh}); % plot catheter
-    plot3(xh,yh,zh,'color',color_arr(rr,:),'linewidth',1,'parent',hh_arr{hh}); % plot helix
-end
-plot(x_pks,y_pks,'.','color',color_arr(rr,:),'markersize',14,'parent',handles.axes3);
-
-for hh = 1:3
-    view(hh_arr{hh},vv_arr(hh,:));
-    axis(hh_arr{hh},'equal');
-    xlabel(hh_arr{hh},'x (mm)');
-    ylabel(hh_arr{hh},'y (mm)');
-    zlabel(hh_arr{hh},'z (mm)');
-    box(hh_arr{hh},'off');
-end
-grid(handles.axes1,'on');
-
-
-% --- Executes on button press in pushbutton_reset.
-function pushbutton_reset_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton_reset (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-set(handles.edit_L,'String',num2str(100));       % length of catheter (mm)
-set(handles.edit_L_act,'String',num2str(40));   % active length (%)
-set(handles.edit_L_res,'String',num2str(0.5));   % number of nodes along catheter length
-   
-set(handles.edit_h_1,'String',num2str(70));     % helix starting point (% length)
-set(handles.edit_h_2,'String',num2str(90));     % helix ending point (% length)
-set(handles.edit_h_n,'String',num2str(100));     % number of points of the helix
-set(handles.edit_h_a,'String',num2str(1));     % amplitude of the sine wave of the helix (mm)
-set(handles.edit_h_w,'String',num2str(5));     % number of sinusoids of the helix
-
-set(handles.edit_x,'String',num2str(0));   % base x-translation 
-set(handles.edit_y,'String',num2str(0));  % base y-translation 
-set(handles.edit_z,'String',num2str(0));  % base z-translation 
-
-set(handles.slider_roll,'Value',0);  % roll (degree)
-set(handles.slider_yaw,'Value',0);   % yaw (degree)
-set(handles.slider_pitch,'Value',0); % pitch (degree)
-set(handles.slider_bend,'Value',1);  % bending (degree)
-
-set(handles.edit_roll,'String',num2str(0));  % roll (degree)
-set(handles.edit_yaw,'String',num2str(0));   % yaw (degree)
-set(handles.edit_pitch,'String',num2str(0)); % pitch (degree)
-set(handles.edit_bend,'String',num2str(1));  % bending (degree)
-
-configure_catheter_and_plot(hObject, eventdata, handles); %%%%%%%% regenerate plot %%%%%%%%
-
-
-
 function edit_yaw_Callback(hObject, eventdata, handles)
 % hObject    handle to edit_yaw (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -823,3 +669,163 @@ function edit_roll_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+
+function clear_axes(hObject, eventdata, handles)
+cla(handles.axes1);
+cla(handles.axes2);
+cla(handles.axes3);
+
+
+
+function configure_catheter_and_plot(hObject, eventdata, handles)
+
+clear_axes(hObject, eventdata, handles);
+
+% define catheter
+L = str2double(get(handles.edit_L,'String'));            % length of catheter (mm)
+L_pct_bent = str2double(get(handles.edit_L_act,'String'));   % active length (%)
+L_res = str2double(get(handles.edit_L_res,'String'));        % number of nodes along catheter length
+
+% define helix
+p1_helix = str2double(get(handles.edit_h_1,'String'));     % helix starting point (% length)
+p2_helix = str2double(get(handles.edit_h_2,'String'));     % helix ending point (% length)
+npt_helix = str2double(get(handles.edit_h_n,'String'));    % number of points of the helix
+a_helix = str2double(get(handles.edit_h_a,'String'));      % amplitude of the sine wave of the helix (mm)
+n_helix = str2double(get(handles.edit_h_w,'String'));      % number of sinusoids of the helix
+
+% define rotation
+th_end = get(handles.slider_bend, 'Value')*pi/180;  % bending (degree)
+alpha = get(handles.slider_roll, 'Value')*pi/180;   % roll (degree)
+beta = get(handles.slider_yaw, 'Value')*pi/180;     % yaw (degree)
+gamma = get(handles.slider_pitch, 'Value')*pi/180;  % pitch (degree)
+M_rot = getRY(gamma)*getRZ(beta)*getRX(alpha);      % the associated rotation matrix
+
+% define translation
+trans_x = str2double(get(handles.edit_x,'String')); % base x-translation
+trans_y = str2double(get(handles.edit_y,'String')); % base y-translation
+trans_z = str2double(get(handles.edit_z,'String')); % base z-translation
+
+% configure catheter
+L2 = 0.01*L_pct_bent*L;
+L1 = L - L2;
+
+Rk = L2/th_end; % radius of curvature of the bent section
+
+x1 = 0:L_res:L1;              % x of unbent
+y1 = zeros(1,length(x1));   % y of unbent
+
+xc = L1;    % x-location of the center of virtual circle
+yc = Rk;    % y-location of the center of virtual circle
+
+th_c = th_end;                   % total angle that the arc spans (rad)
+th_incre = th_c/(L2/L_res);       % angle increment (rad)
+th_angles = (1:(L2/L_res))*th_incre; % arc angle array
+
+x2 = L1 + Rk*sin(th_angles);   % x of bent section
+y2 = Rk - Rk*cos(th_angles);   % y of bent sectoin
+
+X = [x1,x2];            % catheter X coordinate
+Y = [y1,y2];            % catheter Y coordinate
+Z = zeros(1,length(X)); % catheter Z coordinate
+
+M = [X;Y;Z]; % catheter XYZ combined
+
+% configure helix
+pct_helix = p2_helix - p1_helix; % helix global length (% of cathetler L)
+
+% define three segments along the bent length (space 1, helix, space 2) % the three variables below should sum up to 1
+frac_space_1 = (p1_helix - (100 - L_pct_bent))/L_pct_bent;  % ratio of the bent section before helix coverage (a ratio of bent length)
+frac_helix_bent = pct_helix/L_pct_bent;                   % ratio of helix coverage (a ratio of bent length)
+% frac_space_2 = (100 - p2_helix)/L_pct_bent;               % ratio of the bent section after helix coverage (a ratio of bent length)
+
+% identify theta angle from the perspective of the big circle defining curvature
+th_1 = -pi/2 + frac_space_1*th_c;
+th_helix_range = frac_helix_bent*th_c;
+th_2 = th_1 + th_helix_range;
+
+% adjust the total number of sinusoids based on th_helix_range
+n_effect_helix = n_helix*2*pi/th_helix_range;           % effective number of sinusoids (adjusted for the equation)
+
+% define an array for all theta angles along the helix
+th_helix = linspace(th_2,th_1,npt_helix);
+
+% compile helix
+xh = xc + (Rk + a_helix*sin(n_effect_helix*th_helix)).*cos(th_helix);   % x location of helix
+yh = yc + (Rk + a_helix*sin(n_effect_helix*th_helix)).*sin(th_helix);   % y location of helix
+zh = a_helix*cos(n_effect_helix*th_helix);
+
+M_helix = [xh;yh;zh];
+
+% rotate both the catheter and the helix
+M = M_rot*M;
+X = M(1,:); Y = M(2,:); Z = M(3,:);
+M_helix = M_rot*M_helix;
+xh = M_helix(1,:); yh = M_helix(2,:); zh = M_helix(3,:);
+
+% translate both the catheter and the helix
+X = X + trans_x; xh = xh + trans_x;
+Y = Y + trans_y; yh = yh + trans_y;
+Z = Z + trans_z; zh = zh + trans_z;
+
+% find apexes in X-Y projection
+[x_pks,y_pks] = func_find_apex_rot(xh,yh,X,Y,0);
+
+% plot
+c_arr = colormap(lines);
+ii_arr = 1 + [0,length(x1)];
+hh_arr = {handles.axes1,handles.axes2,handles.axes3};
+vv_arr = [-37.5,30; 0,90; 0,90];
+for hh = 1:2
+    hold(hh_arr{hh},'on');
+    plot3(X(ii_arr(hh):end),Y(ii_arr(hh):end),Z(ii_arr(hh):end),'-','color',c_arr(3,:),'linewidth',2,'parent',hh_arr{hh}); % plot catheter
+    plot3(xh,yh,zh,'color',c_arr(1,:),'linewidth',1,'parent',hh_arr{hh}); % plot helix
+end
+for hh = 2:3
+    plot(x_pks,y_pks,'.','color',c_arr(2,:),'markersize',14,'parent',hh_arr{hh});
+end
+
+for hh = 1:3
+    view(hh_arr{hh},vv_arr(hh,:));
+    axis(hh_arr{hh},'equal');
+    xlabel(hh_arr{hh},'x (mm)');
+    ylabel(hh_arr{hh},'y (mm)');
+    zlabel(hh_arr{hh},'z (mm)');
+    box(hh_arr{hh},'off');
+end
+grid(handles.axes1,'on');
+
+
+
+% --- Executes on button press in pushbutton_reset.
+function pushbutton_reset_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton_reset (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+set(handles.edit_L,'String',num2str(100));       % length of catheter (mm)
+set(handles.edit_L_act,'String',num2str(40));   % active length (%)
+set(handles.edit_L_res,'String',num2str(0.5));   % number of nodes along catheter length
+
+set(handles.edit_h_1,'String',num2str(70));     % helix starting point (% length)
+set(handles.edit_h_2,'String',num2str(90));     % helix ending point (% length)
+set(handles.edit_h_n,'String',num2str(100));     % number of points of the helix
+set(handles.edit_h_a,'String',num2str(1));     % amplitude of the sine wave of the helix (mm)
+set(handles.edit_h_w,'String',num2str(5));     % number of sinusoids of the helix
+
+set(handles.edit_x,'String',num2str(0));   % base x-translation
+set(handles.edit_y,'String',num2str(0));  % base y-translation
+set(handles.edit_z,'String',num2str(0));  % base z-translation
+
+set(handles.slider_roll,'Value',0);  % roll (degree)
+set(handles.slider_yaw,'Value',0);   % yaw (degree)
+set(handles.slider_pitch,'Value',0); % pitch (degree)
+set(handles.slider_bend,'Value',1);  % bending (degree)
+
+set(handles.edit_roll,'String',num2str(0));  % roll (degree)
+set(handles.edit_yaw,'String',num2str(0));   % yaw (degree)
+set(handles.edit_pitch,'String',num2str(0)); % pitch (degree)
+set(handles.edit_bend,'String',num2str(1));  % bending (degree)
+
+configure_catheter_and_plot(hObject, eventdata, handles); %%%%%%%% regenerate plot %%%%%%%%
