f79302494a29924845f937c904a189636608532c
aety <ay@u.northwestern.edu>
2018-10-01 17:43:45 +0200

    ran neural net to predict catheter bending and rotation from helical apexes with a range of numbers of parameters describing the apexes

---
+++ results_ffine/nn_neural_fitting_2D_test_predictors_plot_all.m
@@ -0,0 +1,51 @@
+nn_arr = 1:12;
+
+vidflag = 1;
+
+if vidflag
+    opengl('software');
+    anim = VideoWriter(datestr(datetime('now'),'yyyy-mm-dd-HHMMss'),'Motion JPEG AVI');
+    anim.FrameRate = 2;
+    open(anim);
+end
+
+% c_arr = colormap(viridis(length(nn_arr)));
+c_arr = zeros(12,3);
+
+for nn = nn_arr
+    
+    load(['nn_neural_fitting_2D_test_predictors_' num2str(nn)]);
+    
+    hold on;
+    
+    plot(R_arr(:,1),'^','color',c_arr(nn,:));
+    plot(R_arr(:,2),'*','color',c_arr(nn,:));
+    
+    box off;
+    axis tight
+    ylim([0,1]);
+    
+    legend('\theta_{rot}','\theta_{bend}','orientation','horizontal','location','south');
+    xlabel('possible datasets');
+    ylabel('R');
+    title(['n\circ of predictors = ' num2str(nn)],'fontweight','normal');
+    set(gca,'xtick',[0,size(R_arr,1)]);
+    
+    set(gca,'fontsize',14);
+    set(gca,'position',[0.11,0.15,0.78,0.78]);
+    set(gcf,'position',[1800,600,800,400]);
+    set(gcf,'color','w');
+    
+    if vidflag
+        frame = getframe(figure(1));
+        writeVideo(anim,frame);
+    else
+        pause;
+    end
+    clf;
+end
+
+if vidflag
+    close(anim);
+    close;
+end
