56dd07591c3fa7adcaea7ff54a1d0ea86ec5708b
aety <ay@u.northwestern.edu>
2018-10-12 16:47:25 +0200

    deleted old stuff and started editing nn_neural_fitting_2D_pre.m
    Former-commit-id: 327b3e88d0b42fa43fa9c6d147b04e91b870cd09

--- results_ffine/nn_neural_fitting_2D_pre.m
+++ results_ffine/nn_neural_fitting_2D_pre.m
@@ -4,14 +4,12 @@ load circular_approx_curVar_wSine_3D_rotate_findApex;
 %%
 readme = 'Varying bending angles and varying rotation angles';
 
-% x0 = X_PKS_ARR{1,jjj}(end);
-% y0 = Y_PKS_ARR{1,jjj}(end);
-
-txt_arr = {'x_{sum}','y_{sum}','xy_{sum}',...
-    'x_{mean}','y_{mean}','xy_{mean}',...
+pdt_arr = {'x_{mean}','y_{mean}','xy_{mean}',...
     'x_{std}','y_{std}','xy_{std}',...
     'x_{max}','y_{max}','xy_{max}',...
     'x_{min}','y_{min}','xy_{min}'};
+
+rsp_arr = {'\theta_{rot}','\theta_{bend}'};
 
 predictor = nan(size(X_ARR,1),15);
 response = nan(size(X_ARR,1),2);
@@ -23,27 +21,30 @@ for ii = 1:size(X_ARR,1)
         y = Y_PKS_ARR{ii,jj};
         
         nn = ii + (jj-1)*size(X_ARR,1);
-        predictor(nn,1) = sum(x);
-        predictor(nn,2) = sum(y);
-        predictor(nn,3) = sum(sqrt(x.^2 + y.^2));
         
-        predictor(nn,4) = mean(x);
-        predictor(nn,5) = mean(y);
-        predictor(nn,6) = mean(sqrt(x.^2 + y.^2));
+        x_odd = x(1:2:end);
+        x_even = x(2:2:end);
+        y_odd = y(2:2:end);
+        y_even = y(2:2:end);
         
-        predictor(nn,7) = std(x);
-        predictor(nn,8) = std(y);
-        predictor(nn,9) = std(sqrt(x.^2 + y.^2));
+        predictor(nn,1) = mean(x);
+        predictor(nn,2) = mean(y);
+        predictor(nn,3) = mean(sqrt(x.^2 + y.^2));
         
-        predictor(nn,10) = max(x);
-        predictor(nn,11) = max(y);
-        predictor(nn,12) = max(sqrt(x.^2 + y.^2));
+        predictor(nn,4) = std(x);
+        predictor(nn,5) = std(y);
+        predictor(nn,6) = std(sqrt(x.^2 + y.^2));
         
-        predictor(nn,13) = min(x);
-        predictor(nn,14) = min(y);
-        predictor(nn,15) = min(sqrt(x.^2 + y.^2));
+        predictor(nn,7) = max(x);
+        predictor(nn,8) = max(y);
+        predictor(nn,9) = max(sqrt(x.^2 + y.^2));
         
-%         predictor(nn,16) = atan2(y(end)-y0,x(end)-x0);
+        predictor(nn,10) = min(x);
+        predictor(nn,11) = min(y);
+        predictor(nn,12) = min(sqrt(x.^2 + y.^2));
+        
+        % slope change
+        % different sides of the catheter 
         
         response(nn,1) = rot_arr(ii);
         response(nn,2) = variable_arr(jj);
