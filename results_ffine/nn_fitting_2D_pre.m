de77d1f41a05f5ff5e64fc72fbc86a29998a8a95
aety <ay@u.northwestern.edu>
2018-10-17 11:10:42 +0200

    added new predictors
    Former-commit-id: abacd8ad915b19f91c7eaae78f903795e954d6bd

--- results_ffine/nn_fitting_2D_pre.m
+++ results_ffine/nn_fitting_2D_pre.m
@@ -4,61 +4,92 @@ load circular_approx_curVar_wSine_3D_rotate_findApex;
 %%
 readme = 'Varying bending angles and varying rotation angles';
 
-pdt_arr = {'x_{mean}','y_{mean}','xy_{mean}',...
-    'x_{std}','y_{std}','xy_{std}',...
-    'x_{max}','y_{max}','xy_{max}',...
-    'x_{min}','y_{min}','xy_{min}'};
+pdt_txt_arr = {'x_{mean}','y_{mean}','d_{mean}',...
+    'x_{mean,odd}','y_{mean,odd}','d_{mean,odd}',...
+    'x_{mean,even}','y_{mean,even}','d_{mean,even}',...
+    'x_{mean,o-e}','y_{mean,o-e}','d_{mean,o-e}',...
+    'x_{std}','y_{std}','d_{std}',...
+    'x_{std,odd}','y_{std,odd}','d_{std,odd}',...
+    'x_{std,even}','y_{std,even}','d_{std,even}',...
+    'x_{std,o-e}','y_{std,o-e}','d_{std,o-e}',...
+    '\alpha_{mean}','\alpha_{std}',...
+    '\Delta\alpha_{mean}','\Delta\alpha_{std}',...
+    };
 
-rsp_arr = {'\theta_{rot}','\theta_{bend}'};
+rsp_txt_arr = {'\theta_{rot}','\theta_{bend}'};
 
-predictor = nan(size(X_ARR,1),15);
-response = nan(size(X_ARR,1),2);
+predictor = nan(length(pdt_txt_arr),size(X_ARR,1));
+response = nan(length(rsp_txt_arr),size(X_ARR,1));
 
 for ii = 1:size(X_ARR,1)
     for jj = 1:size(X_ARR,2)
         
-        x = X_PKS_ARR{ii,jj};
-        y = Y_PKS_ARR{ii,jj};
-        
+        % counter
         nn = ii + (jj-1)*size(X_ARR,1);
         
-        x_odd = x(1:2:end);
-        x_even = x(2:2:end);
-        y_odd = y(2:2:end);
-        y_even = y(2:2:end);
+        % extract peaks in each frame
+        x = X_PKS_ARR{ii,jj};   % helix peaks x
+        y = Y_PKS_ARR{ii,jj};   % helix peaks y
+        d = sqrt(x.^2 + y.^2);  % helix peaks distance from origin
         
-        predictor(nn,1) = mean(x);
-        predictor(nn,2) = mean(y);
-        predictor(nn,3) = mean(sqrt(x.^2 + y.^2));
-        
-        predictor(nn,4) = std(x);
-        predictor(nn,5) = std(y);
-        predictor(nn,6) = std(sqrt(x.^2 + y.^2));
-        
-        predictor(nn,7) = max(x);
-        predictor(nn,8) = max(y);
-        predictor(nn,9) = max(sqrt(x.^2 + y.^2));
-        
-        predictor(nn,10) = min(x);
-        predictor(nn,11) = min(y);
-        predictor(nn,12) = min(sqrt(x.^2 + y.^2));
+        % separate odd and even indices
+        x_odd = x(1:2:end);     % helix peaks x odd
+        y_odd = y(1:2:end);     % helix peaks y odd
+        d_odd = d(1:2:end);     % helix peaks d odd
+        x_even = x(2:2:end);    % helix peaks x even
+        y_even = y(2:2:end);    % helix peaks x even
+        d_even = d(2:2:end);    % helix peaks d even
         
         % slope change
-        % different sides of the catheter 
+        alpha= atan2(diff(y),diff(x));  % arctangent of local slopes (length = N-1)
+        dalpha = diff(alpha);           % change of the arctangent of local slopes (length = N-2)
         
-        response(nn,1) = rot_arr(ii);
-        response(nn,2) = variable_arr(jj);
+        % list predictors
+        predictor(:,nn) = [mean(x);
+            mean(y);
+            mean(d);
+            mean(x_odd);
+            mean(y_odd);
+            mean(d_odd);
+            mean(x_even);
+            mean(y_even);
+            mean(d_even);
+            mean(x_odd) - mean(x_even);
+            mean(y_odd) - mean(y_even);
+            mean(d_odd) - mean(d_even);
+            std(x);
+            std(y);
+            std(d);
+            std(x_odd);
+            std(y_odd);
+            std(d_odd);
+            std(x_even);
+            std(y_even);
+            std(d_even);
+            std(x_odd) - std(x_even);
+            std(y_odd) - std(y_even);
+            std(d_odd) - std(d_even);
+            mean(alpha);
+            std(alpha);
+            mean(dalpha);
+            std(dalpha)];
+        
+        % list responses
+        response(:,nn) = [rot_arr(ii);
+            variable_arr(jj)];
     end
 end
 
 %%
+n_col_plt = 3;
 hold on;
-c_arr = colormap(plasma(size(predictor,2)));
-for kk = 1:size(predictor,2)
-    subplot(4,4,kk);
-    plot(predictor(:,kk),'*','color',c_arr(kk,:));
-    title(txt_arr{kk},'color',c_arr(kk,:));
+c_arr = colormap(plasma(size(predictor,1)));
+for kk = 1:size(predictor,1)
+    subplot(n_col_plt,ceil(size(predictor,1)/n_col_plt),kk);
+    plot(predictor(kk,:),'.','color',c_arr(kk,:));
+    axis tight;
+    title(pdt_txt_arr{kk},'color',c_arr(kk,:));
 end
 
 %% save
-save nn_fitting_2D_pre predictor response txt_arr readme;
+save nnu_fitting_2D_pre predictor response *_txt_arr readme;
