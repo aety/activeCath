735e36587d44d4324383c08a60342c68193ce319
aetyang <ay@u.northwestern.edu>
2018-10-23 12:17:51 +0200

    minor changes
    Former-commit-id: c997bdb1df9699f365b9298288ce8d9a06fea2a6
    Former-commit-id: 96fce33ae0fb9d3599a7f0f62846e8d446aa6efb

--- catheter_7dof_draw.m
+++ catheter_7dof_draw.m
@@ -1,21 +1,26 @@
+TGL_sav = 1;
+
 % define catheter
 L = 100;            % length of catheter (mm)
-L_pct_bent = 40;   % active length (%)
+L_pct_bent = 40;    % active length (%)
 L_res = 0.5;        % number of nodes along catheter length
 
 % define helix
-p1_helix = 70;     % helix starting point (% length)
-p2_helix = 90;     % helix ending point (% length)
+p1_helix = 70;      % helix starting point (% length)
+p2_helix = 90;      % helix ending point (% length)
 npt_helix = 100;    % number of points of the helix
-a_helix = 1;      % amplitude of the sine wave of the helix (mm)
-n_helix = 5;      % number of sinusoids of the helix
+a_helix = 1;        % amplitude of the sine wave of the helix (mm)
+n_helix = 5;        % number of sinusoids of the helix
 
 % define rotation
-th_end = 1*pi/180;  % bending (degree)
-alpha = 80*pi/180;   % roll (degree)
-beta = 0*pi/180;     % yaw (degree)
-gamma = 0*pi/180;  % pitch (degree)
-M_rot = getRY(gamma)*getRZ(beta)*getRX(alpha);      % the associated rotation matrix
+th_b = 1;           % bending (degree)
+th_r = 80;           % rotation (degree)
+
+th_end = th_b*pi/180;% bending (rad)
+alpha = th_r*pi/180;    % roll (rad)
+beta = 0*pi/180;        % yaw (rad)
+gamma = 0*pi/180;       % pitch (rad)
+M_rot = getRY(gamma)*getRZ(beta)*getRX(alpha); % associated rotation matrix
 
 % define translation
 trans_x = 0; % base x-translation
@@ -23,9 +28,9 @@ trans_y = 0; % base y-translation
 trans_z = 0; % base z-translation
 
 % define camera
-cam_cl = [0,0.4470,0.7410];                         % camera color in RGB
-camAngle = 0*pi/180;  % camera y-rotation angle
-cam_lc = [L/2,0,L/2];                               % camera position
+cam_cl = [0,0.4470,0.7410]; % camera color in RGB
+camAngle = 0*pi/180;        % camera y-rotation angle
+cam_lc = [L/2,0,L/2];       % camera position
 
 % configure catheter
 L2 = 0.01*L_pct_bent*L;
@@ -36,18 +41,18 @@ if th_end==0
 end
 Rk = L2/th_end; % radius of curvature of the bent section
 
-x1 = 0:L_res:L1;              % x of unbent
+x1 = 0:L_res:L1;            % x of unbent
 y1 = zeros(1,length(x1));   % y of unbent
 
 xc = L1;    % x-location of the center of virtual circle
 yc = Rk;    % y-location of the center of virtual circle
 
-th_c = th_end;                   % total angle that the arc spans (rad)
-th_incre = th_c/(L2/L_res);       % angle increment (rad)
+th_c = th_end;              % total angle that the arc spans (rad)
+th_incre = th_c/(L2/L_res); % angle increment (rad)
 th_angles = (1:(L2/L_res))*th_incre; % arc angle array
 
-x2 = L1 + Rk*sin(th_angles);   % x of bent section
-y2 = Rk - Rk*cos(th_angles);   % y of bent sectoin
+x2 = L1 + Rk*sin(th_angles);% x of bent section
+y2 = Rk - Rk*cos(th_angles);% y of bent sectoin
 
 X = [x1,x2];            % catheter X coordinate
 Y = [y1,y2];            % catheter Y coordinate
@@ -60,7 +65,7 @@ pct_helix = p2_helix - p1_helix; % helix global length (% of cathetler L)
 
 % define three segments along the bent length (space 1, helix, space 2) % the three variables below should sum up to 1
 frac_space_1 = (p1_helix - (100 - L_pct_bent))/L_pct_bent;  % ratio of the bent section before helix coverage (a ratio of bent length)
-frac_helix_bent = pct_helix/L_pct_bent;                   % ratio of helix coverage (a ratio of bent length)
+frac_helix_bent = pct_helix/L_pct_bent;                     % ratio of helix coverage (a ratio of bent length)
 % frac_space_2 = (100 - p2_helix)/L_pct_bent;               % ratio of the bent section after helix coverage (a ratio of bent length)
 
 % identify theta angle from the perspective of the big circle defining curvature
@@ -69,7 +74,7 @@ th_helix_range = frac_helix_bent*th_c;
 th_2 = th_1 + th_helix_range;
 
 % adjust the total number of sinusoids based on th_helix_range
-n_effect_helix = n_helix*2*pi/th_helix_range;           % effective number of sinusoids (adjusted for the equation)
+n_effect_helix = n_helix*2*pi/th_helix_range; % effective number of sinusoids (adjusted for the equation)
 
 % define an array for all theta angles along the helix
 th_helix = linspace(th_2,th_1,npt_helix);
@@ -96,10 +101,10 @@ Z = Z + trans_z; zh = zh + trans_z;
 c_arr = colormap(lines); % color array
 ii_arr = 1 + [0,length(x1)]; % index along the catheter to start plotting from
 hh_arr = {subplot(1,3,1),subplot(1,3,2),subplot(1,3,3)}; % subplot handles
-vv_arr = [-37.5,30; 0,90; 0,90]; % view in each subplot
-xx_arr = {'x_0','x_c','x_c'}; % xlabel in each subplot
-yy_arr = {'y_0','y_c','y_c'}; % ylabel in each subplot
-zz_arr = {'z_0','z_c','z_c'}; % zlabel in each subplot
+vv_arr = [-37.5,30; 0,90; 0,90];% view in each subplot
+xx_arr = {'x_0','x_c','x_c'};   % xlabel in each subplot
+yy_arr = {'y_0','y_c','y_c'};   % ylabel in each subplot
+zz_arr = {'z_0','z_c','z_c'};   % zlabel in each subplot
 axcl_arr = {'k',cam_cl,cam_cl}; % axis label color in each subplot
 
 % ------------ plot 1 ------------
@@ -142,15 +147,22 @@ end
 % ------------ configure plots ------------
 for hh = 1:3
     subplot(1,3,hh);
-    view(vv_arr(hh,:));    
+    view(vv_arr(hh,:));
     axis tight;
     axis equal;
     
+    set(gca,'fontsize',12);
     xlabel([xx_arr{hh} ' (mm)'],'color',axcl_arr{hh});
     ylabel([yy_arr{hh} ' (mm)'],'color',axcl_arr{hh});
     zlabel([zz_arr{hh} ' (mm)']);
     box off;
 end
 
-set(gcf,'position',[0,0,1200,400]);
+set(gcf,'position',[0,0,900,300]);
 set(gcf,'color','w');
+
+if TGL_sav
+    set(gcf,'paperposition',[0,0,8,3],'unit','inches');
+    print('-dtiff','-r300',['catheter_7dof_draw_' num2str(th_b) '_' num2str(th_r)]);
+    close
+end
