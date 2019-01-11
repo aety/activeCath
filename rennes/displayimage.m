996f0677979a7f4f7c0876c8d5d620509c39ca13
aetyang <ay@u.northwestern.edu>
2019-01-11 13:01:50 +0100

    first version of peak identification-- using closed areas
    Former-commit-id: 5e53f608dd4319347a33096c5e4ab439a6f56b6b
    Former-commit-id: 0980741c3d291f8804d1145bd2827915551507f5
    Former-commit-id: cfcec74ca5cc499e65096297a5c1ed5e6d97a178

--- rennes/displayimage.m
+++ rennes/displayimage.m
@@ -2,10 +2,12 @@ clear; ca; clc;
 
 flg_plot = 0;
 
+vidflag = 1;
+
 %% load image
+dname = '20SDR-H_30_0135'; % DSA_2_0042
 cd C:\Users\yang\ownCloud\rennes_experiment\18_12_11-09_47_11-STD_18_12_11-09_47_11-STD-160410\__20181211_095212_765000
-% cd DSA_2_0042
-cd 20SDR-H_30_0135
+cd(dname);
 
 fname = dir('*.ima');
 filename = fname.name;
@@ -15,21 +17,29 @@ info = dicominfo(filename);
 
 cd C:\Users\yang\ownCloud\MATLAB\rennes
 
+%% initialize video
+if vidflag
+    opengl('software');
+    anim = VideoWriter([dname '_proc'],'Motion JPEG AVI');
+    anim.FrameRate = 5;
+    open(anim);
+end
+
 %% permute image array
 X3 = permute(X,[1,2,4,3]); % 4D images information usually comprises of Height, Width, Color Plane, Frame Number (Color Plane is in the order Red, Green, Blue)
 
 %% find interesting frames
-th1_bound = [-45,75];
+th1_bound = [-55,65]; % [-55,65];
 th1_arr = info.PositionerPrimaryAngleIncrement;
 ind_arr = find(th1_arr > th1_bound(1) & th1_arr < th1_bound(2));
 
 %% show image (all frames)
 
-for ff = ind_arr(1)%:ind_arr(end) % 35-40,47-56,70-
+for ff = ind_arr(1):ind_arr(end) % 35-40,47-56,70-
     
     % select frame
     I = X3(:,:,ff);
-    I = I(50:920,400:800);
+    I = I(100:920,400:800);
     
     %% plot current
     if flg_plot
@@ -49,51 +59,51 @@ for ff = ind_arr(1)%:ind_arr(end) % 35-40,47-56,70-
     end
     
     %% thresholding using matlab remove background
-    % % %     level = graythresh(J);
-    % % %     K = imbinarize(J,level);
+    level = graythresh(J);
+    K = imbinarize(J,level);
     
     %% thresholding using the histogram
     
-    % no-filter on the histogram
-    y = counts;
-    
-    % find maximum peak and cut off values to its left
-    pksx = find(y==max(y)); % find the x-index of the peak
-    y = y(pksx+1:end);        % only retain values to the right
-    
-    if flg_plot
-        figure(100); hold on; plot(y,'linewidth',1); % plot thresheld histogram
-    end
-    
-    % find local min
-    % %     TF = islocalmin(y); % find local minima
-    % %     temp = 1:256;       % temporary array for x-indices
-    % %     lminx = temp(TF);   % x-values of local minima
-    % %     lminy = y(TF);      % y-values of local minima
+    % %     % no-filter on the histogram
+    % %     y = counts;
+    % %
+    % %     % find maximum peak and cut off values to its left
+    % %     pksx = find(y==max(y)); % find the x-index of the peak
+    % %     y = y(pksx+1:end);        % only retain values to the right
     % %
     % %     if flg_plot
-    % %         figure(100); plot(lminx,lminy,'*','linewidth',5); % plot local minima
+    % %         figure(100); hold on; plot(y,'linewidth',1); % plot thresheld histogram
     % %     end
-    
-    % mirror the first inflection point on the right of the peak to the left
-    % %     ind_infl = find(diff(lminy)>0,1);       % find index of inflection point
-    % %     if isempty(ind_infl)
-    % %         ind_infl = length(lminy);           % take the last point if no inflection
+    % %
+    % %     % find local min
+    % %     % %     TF = islocalmin(y); % find local minima
+    % %     % %     temp = 1:256;       % temporary array for x-indices
+    % %     % %     lminx = temp(TF);   % x-values of local minima
+    % %     % %     lminy = y(TF);      % y-values of local minima
+    % %     % %
+    % %     % %     if flg_plot
+    % %     % %         figure(100); plot(lminx,lminy,'*','linewidth',5); % plot local minima
+    % %     % %     end
+    % %
+    % %     % mirror the first inflection point on the right of the peak to the left
+    % %     % %     ind_infl = find(diff(lminy)>0,1);       % find index of inflection point
+    % %     % %     if isempty(ind_infl)
+    % %     % %         ind_infl = length(lminy);           % take the last point if no inflection
+    % %     % %     end
+    % %
+    % %     % mirror the minimum on the right by its y-value
+    % %     %     cut_y = lminy(ind_infl);                    % find the y-value of the cutoff point on the right
+    % %     cut_y = min(y);
+    % %     cut_x_temp = find(counts(1:pksx) > cut_y);  % find (left) values greater than the cutoff
+    % %     temp = find(diff(cut_x_temp)>1,1,'last');   % find the first point of the last bunch greater than the cutoff
+    % %     cut_pt = cut_x_temp(temp+1);                % find the x-value of the cutoff point
+    % %
+    % %     if flg_plot
+    % %         figure(99); plot(cut_pt,counts(cut_pt),'o','linewidth',5); % plot cutoff
     % %     end
-    
-    % mirror the minimum on the right by its y-value
-    %     cut_y = lminy(ind_infl);                    % find the y-value of the cutoff point on the right
-    cut_y = min(y);
-    cut_x_temp = find(counts(1:pksx) > cut_y);  % find (left) values greater than the cutoff
-    temp = find(diff(cut_x_temp)>1,1,'last');   % find the first point of the last bunch greater than the cutoff
-    cut_pt = cut_x_temp(temp+1);                % find the x-value of the cutoff point
-    
-    if flg_plot
-        figure(99); plot(cut_pt,counts(cut_pt),'o','linewidth',5); % plot cutoff
-    end
-    
-    fr = [0,cut_pt]/256;
-    K = imadjust(J,fr);
+    % %
+    % %     fr = [0,cut_pt]/256;
+    % %     K = imadjust(J,fr);
     
     %% plot current image
     [countsK,~] = imhist(K); % saturation should occur on the furthest left and right
@@ -107,12 +117,39 @@ for ff = ind_arr(1)%:ind_arr(end) % 35-40,47-56,70-
     % % %     figure; imshow(BW);
     
     %% regionprops
-    s = regionprops(K,'centroid');
-    centroids = cat(1, s.Centroid);
-    hold on; plot(centroids(:,1),centroids(:,2),'*');
+    % Centroid
+    % %     cc = bwconncomp(K);
+    % %     s = regionprops('table',cc,'Centroid');
+    % %     Centroid = cat(1, s.Centroid);
+    % %     hold on; plot(Centroid(:,1),Centroid(:,2),'o','markersize',5,'markerfacecolor','g','markeredgecolor','g');
+    
+    c_arr = lines(3);
+    
+    % BoundingBox
+    cc = bwconncomp(K);
+    s = regionprops('table',cc,'BoundingBox');
+    BoundingBox = cat(1, s.BoundingBox);
+    BoundingBox(BoundingBox(:,4)>400,:) = [];
+    %     for ii = 1:size(BoundingBox,1)
+    %         hold on; rectangle('Position',BoundingBox(ii,:),'facecolor','r','edgecolor','r');
+    %     end
+    temp = BoundingBox(:,1:2);
+    hold on; plot(temp(:,1),temp(:,2),'o','markersize',5,'markerfacecolor',c_arr(2,:),'markeredgecolor',c_arr(2,:));
+    
     
     %%
-    title(th1_arr(ff));
+    title(['\theta_{roll} = ' num2str(th1_arr(ff))]);
     set(gcf,'position',[1,581,1600,737]);
-    pause(0.001);
+    %     pause(0.001);
+    if vidflag
+        frame = getframe(figure(1000));
+        writeVideo(anim,frame);
+        clf;
+    end
+end
+
+%% close video
+if vidflag
+    close(anim);
+    close;
 end
