883eafff2c3a8a1eb49b4b5a082557077fb790ca
aetyang <ay@u.northwestern.edu>
2019-01-21 18:24:41 +0100

    improved convex back peak identification
    Former-commit-id: 9a608d03e67d7077d5327c79878ccd20ca9e1acd
    Former-commit-id: a27c2ca8cb7c33222259e92d9f741aec1c32f589
    Former-commit-id: 1e8c838aed25289e4da4650f277d4caa8d825794

--- rennes/displayimage_20SDR_H_30_short_auto.m
+++ rennes/displayimage_20SDR_H_30_short_auto.m
@@ -7,30 +7,27 @@ vidrate = 20; % video frame rate
 
 % figure parameters
 c_arr = lines(5); c_lab_1 = c_arr(2,:); c_lab_2 = c_arr(5,:); % marker color label
-msize = 5; % markersize
-lwd = 1; % linewidth
-ht = 600; % figure height (pixels)
+msize = 10; % markersize
+lwd = 2; % linewidth
+ht = 800; % figure height (pixels)
 txt_d = 30; % distance of labeling text from the edge (pixels)
 txt_s = 14; % font size
 
 % image processing parameters
-th1_range = [-75,85]; % range of "roll" rotations to include
+th1_range = [-75,75]; % range of "roll" rotations to include
 plt_range = [201,850,251,800]; % range of pixels to plot ([x_0,x_end,y_0,y_end])
-sharp_r = 3; % sharpening radius
+sharp_r = 2; % sharpening radius
 sharp_a = 3; % sharpening amount
 thrs_small = 100; % (pixels)^2 threshold for removing small objects (during arbitrary stage; helps remove tip positioning boxes)
 thrs_big = 50; % (pixels)^2 threshold for removing oversized identified area (catheter diameter is about 10 pixels)
 thrs_dev = 10; % (pixels) maximum deviation from the catheter (fitted curve) an identified point is allowed to be (wire envelopes are about 5 pixels outside of the catheter)
 y_min = 550; % (pixels) vertical pixel location of the lowest interesting extracted features (to avoid inclusion of the catheter base holder)
 
-
-
-
 %% load image
 
 dname_arr = {'20SDR-H_30_0003','20SDR-H_30_0021','20SDR-H_30_0067','20SDR-H_30_0083','20SDR-H_30_0099'}; %
 
-for dd = 1:length(dname_arr)
+for dd = 5%1:length(dname_arr)
     
     dname = dname_arr{dd};
     cd C:\Users\yang\ownCloud\rennes_experiment\18_12_11-09_47_11-STD_18_12_11-09_47_11-STD-160410\__20181211_095212_765000
@@ -101,7 +98,7 @@ for dd = 1:length(dname_arr)
         if pltflag
             figure;
             imshow(I_rsm);
-            title('bwareaopen');
+            title('remove small objects');
         end
         
         %% identify catheter
@@ -111,7 +108,7 @@ for dd = 1:length(dname_arr)
         PixelIdxList = s.PixelIdxList;
         [~,ind] = max(BoundingBox(:,4));
         bbox_big = BoundingBox(ind,:);
-        bbox_big= round(bbox_big); % round to the nearest pixel integer
+        bbox_big= floor(bbox_big); % round to the nearest pixel integer
         
         if pltflag
             figure;
@@ -126,75 +123,45 @@ for dd = 1:length(dname_arr)
         end
         
         %% exclude smaller boxes (I_exsb)
-        bbox_rest = BoundingBox; bbox_rest(ind,:) = [];
-        tgl = zeros(size(I_rsm));
-        for bb = 1:size(bbox_rest,1)
-            temp = bbox_rest(bb,:);
-            bxlim = round(temp); % round to the nearest pixel integer
-            tgl(bxlim(2):bxlim(2)+bxlim(4),bxlim(1):bxlim(1)+bxlim(3)) = 1;
-        end
-        tgl = logical(tgl);
-        I_exsb = I_dtr;
-        I_exsb(tgl) = 0; % turn irrelevant area in original image black
-        
-        if pltflag
-            figure;
-            imshow(I_exsb);
-            hold on;
-            title('exclude small boxes');
-        end
-        
-        %% fit a curve to the catheter
-        [x,y] = find(I_exsb==1);
-        p = polyfit(x,y,2); % y = p(1)*x^2 + p(2)*x + p(3)
-        
-        a = linspace(min(x),max(x),100);
-        b = polyval(p,a);
-        if pltflag
-            plot(b,a,'linewidth',lwd);
-        end
-        
-        %% retain bounding box of catheter only (I_ctol)
+        %         bbox_rest = BoundingBox; bbox_rest(ind,:) = [];
+        %         tgl = zeros(size(I_rsm));
+        %         for bb = 1:size(bbox_rest,1)
+        %             temp = bbox_rest(bb,:);
+        %             bxlim = round(temp); % round to the nearest pixel integer
+        %             tgl(bxlim(2):bxlim(2)+bxlim(4),bxlim(1):bxlim(1)+bxlim(3)) = 1;
+        %         end
+        %         tgl = logical(tgl);
+        %         I_exsb = I_dtr;
+        %         I_exsb(tgl) = 0; % turn irrelevant area in original image black
+        %
+        %         if pltflag
+        %             figure;
+        %             imshow(I_exsb);
+        %             hold on;
+        %             title('exclude small boxes');
+        %         end
+                
+        %% retain bounding box of catheter only (I_ctol) and binarize
         tgl = zeros(size(I_dtr));
         tgl(bbox_big(2):bbox_big(2)+bbox_big(4),bbox_big(1):bbox_big(1)+bbox_big(3)) = 1;
-        L = I_shp;
         
+        L = I_shp;
         level = graythresh(L(bbox_big(2):bbox_big(2)+bbox_big(4),bbox_big(1):bbox_big(1)+bbox_big(3)));
         I_ctol = imbinarize(L,level);
+        %         thresh = multithresh(L,3);
+        %         seg_I = imquantize(L,thresh);
+        %         I_ctol = seg_I;
+        %         I_ctol(I_ctol>=3) = 0;
         I_ctol(~tgl) = 1;
         
         if pltflag
             figure;
-            imshow(I_ctol);
-            title('exclude outside');
+            imshow(I_ctol,[]);
+            title('exclude outside (from sharpen) and binarize');
         end
         
-        %% regionprops-- boundingbox (for convex front)
-        
-        % BoundingBox
-        cc = bwconncomp(I_ctol,4); %  returns the connected components CC found in the binary image BW % 4 is the specification of connectivity
-        s = regionprops('table',cc,'Centroid','BoundingBox','Area');
-        Centroid = s.Centroid;
-        Area = s.Area;
-        BoundingBox = s.BoundingBox;
-        
-        % Exclude big boxes, low centroids, and outliers from the catheter
-        tgl_exc = zeros(size(Centroid,1),1);
-        tgl_exc(Area > thrs_big) = 1; % remove identified boxes that are much bigger than an "envelope" size
-        tgl_exc(Centroid(:,2) > y_min) = 1;
-        %         test = abs(p(1)*Centroid(:,2).^2 + p(2)*Centroid(:,2) + p(3) - Centroid(:,1));
-        %         tgl_exc(test > thrs_dev,:) = 1; % remove points that deviate too much from the catheter (fitted curve)
-        BoundingBox(logical(tgl_exc),:) = [];
-        Centroid(logical(tgl_exc),:) = [];
-        
-        xx1 = Centroid(:,1); yy1 = Centroid(:,2);
-        
-        %% edge and regionprops (for convex back)
-        % edge
-        BW = I_ctol;
-        BW= edge(BW,'Canny');
-        
-        % regionprops
+        %% ConvexHull regionprops (for convex back)
+        BW = imcomplement(I_ctol);
         BW(y_min:end,:) = 0;
         s = regionprops('table',BW,'ConvexHull','ConvexArea');
         ConvexHull = s.ConvexHull;
@@ -203,6 +170,33 @@ for dd = 1:length(dname_arr)
         ConvexHull = ConvexHull{ind};
         
         xx2 = ConvexHull(:,1); yy2 = ConvexHull(:,2);
+        
+        % remove points that don't qualify
+        tgl_2 = false(1,length(xx2));
+        tgl_2(abs(yy2-y_min) < 5) = 1; % points that are at the bottom of the selected region
+        temp = sqrt((xx2-xx2(1)).^2 + (yy2-yy2(1)).^2);
+        tgl_2(abs(temp) < 18) = 1; % points that are too close to the first point (upper left)
+        temp = sqrt(diff(xx2).^2+diff(yy2).^2);
+        tgl_2(abs(temp)<1) = 2; % points that are too close to each other
+        xx2(tgl_2) = []; yy2(tgl_2) = [];
+        
+        %% BoundingBox regionprops(for convex front)
+        
+% % %         % BoundingBox
+% % %         cc = bwconncomp(I_shp,4); %  returns the connected components CC found in the binary image BW % 4 is the specification of connectivity
+% % %         s = regionprops('table',cc,'Centroid','BoundingBox','Area');
+% % %         Centroid = s.Centroid;
+% % %         Area = s.Area;
+% % %         BoundingBox = s.BoundingBox;
+% % %         
+% % %         % Exclude big boxes, low centroids, and outliers from the catheter
+% % %         tgl_exc = zeros(size(Centroid,1),1);
+% % %         tgl_exc(Area > thrs_big) = 1; % remove identified boxes that are much bigger than an "envelope" size
+% % %         tgl_exc(Centroid(:,2) > y_min) = 1;
+% % %         BoundingBox(logical(tgl_exc),:) = [];
+% % %         Centroid(logical(tgl_exc),:) = [];
+% % %         
+% % %         xx1 = Centroid(:,1); yy1 = Centroid(:,2);
         
         %% main plot
         % figure sizing
@@ -213,9 +207,8 @@ for dd = 1:length(dname_arr)
         I_disp = imadjust(I_str,[0,level*2]);
         imshow(I_disp);
         hold on;
-        %         plot(xx1,yy1,'o','linewidth',lwd,'color',c_lab_1,'markersize',msize);
-        %         plot(xx2,yy2,'o','linewidth',lwd,'color',c_lab_2,'markersize',msize);
-        rectangle('Position',bbox_big,'edgecolor',c_lab_1,'linewidth',lwd);
+        %         plot(xx1,yy1,'*','linewidth',lwd,'color',c_lab_1,'markersize',msize);
+        plot(xx2,yy2,'*-','linewidth',lwd,'color',c_lab_2,'markersize',msize);
         
         text(txt_d,size(I_str,1)-txt_d,['\theta_{roll} = ' num2str(th1_arr(ff))],'fontsize',txt_s);
         
@@ -225,7 +218,7 @@ for dd = 1:length(dname_arr)
             writeVideo(anim,frame);
             clf;
         else
-            pause(0.001);
+            pause(0.1);
         end
     end
     
