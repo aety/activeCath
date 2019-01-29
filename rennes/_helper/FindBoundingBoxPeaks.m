ff8f9d43a402f702b1d18e711f8824092d599958
aetyang <ay@u.northwestern.edu>
2019-01-29 15:16:51 +0100

    revised FindBoundingBoxPeaks.m to replace dsearchn with knnsearch
    Former-commit-id: d08525cd233dade65f0226c34426452e48125153
    Former-commit-id: 7e3b63f6901d3aa5d62030f49bafdcd4be40dde8
    Former-commit-id: f5ff98ad1b302533ab815defc37757af4713bc93

--- rennes/_helper/FindBoundingBoxPeaks.m
+++ rennes/_helper/FindBoundingBoxPeaks.m
@@ -1,6 +1,4 @@
 function P = FindBoundingBoxPeaks(BoundingBox,Centroid,MajorAxisLength,MinorAxisLength,Orientation,x,y)
-% clear; clc; ca;
-% load test_find_BoundingBoxPeaks BoundingBox Centroid MajorAxisLength MinorAxisLength Orientation x y
 
 tgl_plt = 0;
 el_npt = 50; % number of points of ellipses
@@ -8,20 +6,11 @@ el_npt = 50; % number of points of ellipses
 bb_n = length(BoundingBox);
 P = nan(bb_n,2);
 
+x = interp1(1:100,x,1:0.1:100);
+y = interp1(1:100,y,1:0.1:100);
+
 for ii = 1:length(BoundingBox)
-    
-    %% boundingbox
-%     bb_pixels = BoundingBox(ii,1:2); % bottom-left corner of bounding boxes
-%     bb_x = nan(BoundingBox(ii,3),BoundingBox(ii,4)); bb_y = bb_x; % preallocate
-%     for aa = 1:BoundingBox(ii,3)
-%         for bb = 1:BoundingBox(ii,4)
-%             bb_x(aa,bb) = bb_pixels(1) + (aa-1); % contruct a 2D array describing pixel locations
-%             bb_y(aa,bb) = bb_pixels(2) + (bb-1); % contruct a 2D array describing pixel locations
-%         end
-%     end
-%     bb_x(~FilledImage{ii}) = nan; % remove empty pixels
-%     bb_y(~FilledImage{ii}) = nan; % remove empty pixels
-    
+        
     %% ellipse
     t = linspace(0,2*pi,el_npt);    % array of angular variations
     a = MajorAxisLength(ii)/2;      % major axis
@@ -40,8 +29,7 @@ for ii = 1:length(BoundingBox)
     
     XI = [y(k1);x(k1)]';            % subject point (the nearest point on the catheter from the centroid)
     X = [xe;ye]'; X(end,:) = [];    % object points (ellipse) (eliminating the last row to avoid repetitions)
-    T = delaunayn(X,{'Qt','Qbb','Qz'}); % calculate Delaunay Triangulation (changed last option too 'Qz' to resolve co-circular problem)
-    k2 = dsearchn(X,T,XI);              % search for the point on the ellipse closest to the catheter
+    k2 = knnsearch(X,XI);
     k2 = k2 + el_npt/2;                 % Search for the point on the ellipse furthest to the catheter (opposite to the previous point)
     k2(k2>el_npt) = k2(k2>el_npt) - el_npt; % round the index down if over the number of elements
     
@@ -52,7 +40,7 @@ for ii = 1:length(BoundingBox)
         
         plot(Xc,Yc,'.m'); % centroid of bounding boxes
         plot(y(k1),x(k1),'*k'); % nearest point on catheter
-%         plot(bb_x,bb_y,'.b') % pixels in bounding boxes
+        plot(bb_x,bb_y,'.b') % pixels in bounding boxes
         plot(xe,ye,'m') % ellipses                        
         plot(xe(k2),ye(k2),'*r'); % furthest point on the ellipse
     end
