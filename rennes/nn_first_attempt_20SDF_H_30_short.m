5987b650e18da14133699411c19b625aa70fae79
aetyang <ay@u.northwestern.edu>
2019-01-29 16:49:45 +0100

    started a file to play round with preparation for nn fit
    Former-commit-id: 5f1b4c44ac2636a5c2445a08957bc00a3a37a14c
    Former-commit-id: c0b2bfe4c352c11d56cc2ced46c0c048f9c8b1da
    Former-commit-id: d3e6ee0397b79a04d09f362304b22cca63354bc0

--- rennes/nn_first_attempt_20SDF_H_30_short.m
+++ rennes/nn_first_attempt_20SDF_H_30_short.m
@@ -2,11 +2,11 @@ clear; ca; clc;
 load proc_auto_data_20SDR-H_30_0003;
 
 %%
-
-for ii = 1:length(TGL)
-    
+cmap = colormap(parula(length(TGL)));
+for ii = 1:length(TGL)    
     hold on;
-    plot(Y(end,ii),X(end,ii),'*');
-    axis equal
-    
+    bbox = BBOX{ii};
+    f = scatter(bbox(:,1),bbox(:,2),20,cmap(ii,:),'filled');
+    alpha(f,0.5);
+
 end
