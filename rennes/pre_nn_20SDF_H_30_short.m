e5672fb6194dfdd4dda6966c084aed78ad7b88f3
aetyang <ay@u.northwestern.edu>
2019-02-15 19:05:46 +0100

    added PeaksInterp and reran post-processing and nn
    Former-commit-id: 4c0f8de75d4fd40a540494ced54e45ae1946b7e2
    Former-commit-id: 37ad18ff15c165780155a4bd6ba3b2e756100f68
    Former-commit-id: ba63fa40c90b6a9dfcae4cc994799bb502373167

--- rennes/pre_nn_20SDF_H_30_short.m
+++ rennes/pre_nn_20SDF_H_30_short.m
@@ -12,15 +12,16 @@ cmap1 = RdBu;
 cmap2 = BrBG;
 
 tgl_cbar = 0;
-tgl_plot = 1;
-tgl_svpl = 1;
+tgl_plot = 0;
+tgl_svpl = 0;
 tgl_save = 1;
 
 load(['proc_auto_data_' dname_arr{1}],'ind_arr');
 load th1_arr
 
 %% name predictors and responses
-PDT_txt = {'Y_0','max(\Delta\alpha)','max(\alpha_{end})','mean(\Delta\alpha)','std(\Delta\alpha)','std(\alpha)','CV(d_{lat})','diff(mean(d_{lat}))'};
+% PDT_txt = {'Y_0','max(\Delta\alpha)','max(\alpha_{end})','mean(\Delta\alpha)','std(\Delta\alpha)','std(\alpha)','CV(d_{lat})','diff(mean(d_{lat}))'};
+PDT_txt = {'Y_0','max(\Delta\alpha)','mean(\Delta\alpha)','std(\Delta\alpha)','std(\alpha)','CV(d_{lat})'};
 RSP_txt = {'\theta_{roll}','\theta_{bend}'};
 
 %% select only a certain range
@@ -56,6 +57,8 @@ for dd = 1:n_bend
     %% loop through frames
     for ii = idx1:idx2
         
+        disp([dd,ii]);
+        
         % load data
         tgl = TGL{ii};
         ref = REF(:,ii);
@@ -64,29 +67,24 @@ for dd = 1:n_bend
         % subtract reference points to get relative positions
         PXY = repmat(ref',length(PXY),1) - PXY;
         
-        %         % remove distal points
-        %         tgl_dis = rssq(PXY') > cath_len;
-        %         PXY(tgl_dis,:) = nan;
-        
         % remove NaN's
         tgl(isnan(PXY(:,1))) = [];
         PXY(isnan(PXY(:,1)),:) = [];
         
-        % separate-- set 1
+        % separate-- set 1 -- inner
         pxy1 = PXY(tgl,:);
         pxy1 = sortrows(pxy1,2);
-        %         tgl_near = RemoveOverlap(pxy1,m_dist);  % remove points that are too close
-        %         pxy1(~tgl_near,:) = [];                 % remove points that are too close
         n = min([n_pks,size(pxy1,1)]);  % pick the smaller between the actual and defined number of peaks
         plt1 = nan(n_pks,2);            % preallocate
         plt1(1:n,:) = pxy1(1:n,:);      % find the first n peaks
         
-        % separate-- set 2
+        % separate-- set 2 -- outer
         pxy2 = PXY(~tgl,:);
         pxy2 = sortrows(pxy2);
-        %         tgl_near = RemoveOverlap(pxy2,m_dist);  % remove points that are too close
-        %         pxy2(~tgl_near,:) = [];                 % remove points that are too close
         n = min([n_pks,size(pxy2,1)]);  % pick the smaller between the actual number of peaks and defined threshold
+        
+        pxy1 = PeaksInterp(pxy1); %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% interpolate gaps %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
         plt2 = nan(n_pks,2);            % preallocate
         plt2(1:n,:) = pxy2(1:n,:);      % find the first n peaks
         
@@ -111,35 +109,40 @@ for dd = 1:n_bend
         end
         
         
-        %% compile NN predictors
+        %% compile NN predictors (set 1 -- on the right) (set 2 -- on the left)
         nn = nn + 1; % counter
         
         slc = plt2(1,2) < plt1(1,2);    % pick a point at the lowest y-position
         temp = [plt1(1,2),plt2(1,2)];   % pick a point at the lowest y-position
         
-        slp1 = diff(plt1(:,2))./diff(plt1(:,1)); % local slope between adjacent points (set 1)
+        %         slp1 = diff(plt1(:,2))./diff(plt1(:,1)); % local slope between adjacent points (set 1)
         slp2 = diff(plt2(:,2))./diff(plt2(:,1)); % local slope between adjacent points (set 2)
         
-        temp1 = plt1; temp1(isnan(plt1(:,1)),:) = []; temp1 = temp1([1,end],:); temp1 = diff(temp1); % x and y distances bewteen the first and last points (set 1)
-        temp2 = plt2; temp2(isnan(plt2(:,1)),:) = []; temp2 = temp2([1,end],:); temp2 = diff(temp2); % x and y distances bewteen the first and last points (set 2)
-        mslpend = max(abs([temp1(2)/temp1(1),temp2(2)/temp2(1)])); % take the (absolute) slope between the first and last points and pick the greater one out of the two
+        %         temp1 = plt1; temp1(isnan(plt1(:,1)),:) = []; temp1 = temp1([1,end],:); temp1 = diff(temp1); % x and y distances bewteen the first and last points (set 1)
+        %         temp2 = plt2; temp2(isnan(plt2(:,1)),:) = []; temp2 = temp2([1,end],:); temp2 = diff(temp2); % x and y distances bewteen the first and last points (set 2)
+        %         mslpend = max(abs([temp1(2)/temp1(1),temp2(2)/temp2(1)])); % take the (absolute) slope between the first and last points and pick the greater one out of the two
         
-        dlat1 = sqrt(diff(plt1(:,2)).^2 + diff(plt1(:,1)).^2); % local lateral distance (set 1)
+        %         dlat1 = sqrt(diff(plt1(:,2)).^2 + diff(plt1(:,1)).^2); % local lateral distance (set 1)
         dlat2 = sqrt(diff(plt2(:,2)).^2 + diff(plt2(:,1)).^2); % local lateral distance (set 2)
-        dlat = [dlat1;dlat2]; % all local distances (both sets)
-        diffdlat = abs(diff([nanmean(dlat1),nanmean(dlat2)]));
+        %         dlat = [dlat1;dlat2]; % all local distances (both sets)
+        dlat = dlat2; % all local distances (both sets)
+        %         diffdlat = abs(diff([nanmean(dlat1),nanmean(dlat2)]));
         
         PDT(1,nn) = temp(slc+1);                            % predictor 1 -- Y0
-        PDT(2,nn) = nanmax(abs([diff(slp1);diff(slp2)]));   % predictor 2 -- maximum local slope change
-        PDT(3,nn) = mslpend;                                % predictor 3 -- absolute slope between the first and last points
-        PDT(4,nn) = nanmean([diff(slp1);diff(slp2)]);       % predictor 4 -- average of local slope change
-        PDT(5,nn) = nanstd([diff(slp1);diff(slp2)]);        % predictor 5 -- standard deviation of ALL local slope change
-        PDT(6,nn) = nanstd([slp1;slp2]);                    % predictor 6 -- standard deviation of ALL local slope
-        PDT(7,nn) = nanstd(dlat)/nanmean(dlat);             % predictor 7 -- coefficient of variation of ALL lateral distances
-        PDT(8,nn) = diffdlat;                               % predictor 8 -- absolute difference between average lateral distances of set 1 and set 2
+        PDT(2,nn) = nanmax(abs(diff(slp2)));   % predictor 2 -- maximum local slope change
+        %         PDT(2,nn) = nanmax(abs([diff(slp1);diff(slp2)]));   % predictor 2 -- maximum local slope change
+        %         PDT(3,nn) = mslpend;                                % predictor 3 -- absolute slope between the first and last points
+        %         PDT(4,nn) = nanmean([diff(slp1);diff(slp2)]);       % predictor 4 -- average of local slope change
+        PDT(3,nn) = nanmean(diff(slp2));                  % predictor 4 -- average of local slope change
+        %         PDT(5,nn) = nanstd([diff(slp1);diff(slp2)]);        % predictor 5 -- standard deviation of ALL local slope change
+        PDT(4,nn) = nanstd(diff(slp2));                   % predictor 5 -- standard deviation of ALL local slope change
+        %         PDT(6,nn) = nanstd([slp1;slp2]);                    % predictor 6 -- standard deviation of ALL local slope
+        PDT(5,nn) = nanstd(slp2);                           % predictor 6 -- standard deviation of ALL local slope
+        PDT(6,nn) = nanstd(dlat)/nanmean(dlat);             % predictor 7 -- coefficient of variation of ALL lateral distances
+        %         PDT(7,nn) = diffdlat;                               % predictor 8 -- absolute difference between average lateral distances of set 1 and set 2
         
         RSP(1,nn) = th1_arr(ind_arr(ii));   % response 1 -- roll angle
-        RSP(2,nn) = th_bend_act;           % response 2 -- bend angle
+        RSP(2,nn) = th_bend_act;            % response 2 -- bend angle
         
         XY(:,nn) = [plt1(:,1);plt1(:,2);plt2(:,1);plt2(:,2)]; % master storage for all all x-y points
         
@@ -168,7 +171,6 @@ for dd = 1:n_bend
             close;
         end
     end
-    
 end
 
 if tgl_save
