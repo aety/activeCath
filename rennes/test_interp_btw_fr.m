c5144f45d0e9bed672a08701e710c99e6a066d68
aetyang <ay@u.northwestern.edu>
2019-03-08 17:00:49 +0100

    first smoothing results
    Former-commit-id: 294b912c7ed28d222a23c96ada7499f16b3a6047
    Former-commit-id: bddc863d76075f98d0e69743afb865812b0df8e5
    Former-commit-id: d5ed0bbdc713b0f9b03cf372e2d5b2c04c819d02

--- rennes/test_interp_btw_fr.m
+++ rennes/test_interp_btw_fr.m
@@ -98,7 +98,7 @@ for dd = 1:n_bd % only good between 1 and 3
         B_fr = reshape(lab_fr,n_pt*n_fr,1);     % reshape
         
         B_pk = reshape(pk,n_pt*n_fr,2);         % reshape
-                
+        
         temp = [nanmin(B_pk(:,2)),nanmax(B_pk(:,2))];   % save original scale
         B_pk(:,2) = rescale(B_pk(:,2),0,1000000);    % scale to y-dimension to help clustering
         [idx,C,sumd,D] = kmeans(B_pk,n_cl,'replicate',10,'Display','final','start','plus','EmptyAction','error','Distance','sqeuclidean'); % cluster
@@ -124,13 +124,14 @@ if plt
             
             B_pk = M_pk(:,:,pp,dd);
             idx = M_idx(:,pp,dd);
+            n_cl = n_cl_arr(pp);
             
             figure;
+            cmap = colormap(lines(n_cl));
+            hold on;
             
             for cc = 1:n_cl
                 tgl = idx==cc;
-                cmap = colormap(lines(n_cl));
-                hold on;
                 %                                 c = 'r';     % diagnositc
                 c = cmap(cc,:);
                 h = scatter(B_pk(tgl,1),B_pk(tgl,2),10,c,'filled');
@@ -140,6 +141,66 @@ if plt
         end
     end
 end
+
+
+
+%% smoothing attempy 1
+clear; clc; ca;
+load test_interp_btw_fr
+
+% -----------------------------------------------------------
+% manually correct false cluster
+test = M_pk(:,1,1,5)> - 61 & M_pk(:,2,1,5)>288;
+M_idx(test,1,5) = 4;
+% -----------------------------------------------------------
+tt_txt = {'concave','concave'};
+
+for dd = 1:n_bd
+    
+    for pp = 1:2
+        
+        %         subplot(1,2,pp);
+        hold on;
+        
+        n_cl = n_cl_arr(pp);
+        
+        B_pk = M_pk(:,:,pp,dd);
+        idx = M_idx(:,pp,dd);
+        
+        B_node = M_node(:,dd);
+        B_fr = M_fr(:,dd);
+        
+        cmap = colormap(lines(n_cl));
+        
+        for cc = 1:n_cl
+            tgl = idx==cc;
+            
+            p1 = polyfit(B_pk(tgl,1),B_pk(tgl,2),2);
+            x = B_pk(tgl,1);
+            y = polyval(p1,x);
+            
+            c = cmap(cc,:);
+            h = scatter(B_pk(tgl,1),B_pk(tgl,2),2,c,'filled');
+            alpha(h,0.1);
+            plot(x,y,'color',c);
+            %             text(-10+x(end),y(end),num2str(cc));
+        end
+        %         title(tt_txt{pp},'fontweight','normal');
+        
+    end
+    set(gca,'position',[0,0,1,1]);
+    set(gca,'fontsize',6);
+    axis equal
+    ylim([0,450]);
+    axis off
+    
+    set(gcf,'paperposition',[0,0,2.5,3],'unit','inches');
+    print('-dtiff','-r300',['test_interp_byNode_smooth_' num2str(dd)]);
+    close;
+    %     pause;
+    %     clf;
+end
+
 
 %% alternative clustering
 %     GMModel = fitgmdist(B_pk1,16,'Options',statset('MaxIter',500),'CovarianceType','diagonal','SharedCovariance',true,'RegularizationValue',0.01);
