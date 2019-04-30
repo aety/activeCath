d9ab6d21182d1fbb1045ca9bd79c2f007cb0d17c
aetyang <ay@u.northwestern.edu>
2019-04-30 14:40:42 +0200

    added normalization to nn_training.m
    Former-commit-id: 915569c5f611425e1b9385444fd6ece4fbbcbbf4
    Former-commit-id: b0b91747e95396d632e52867e475f7538957a9bf
    Former-commit-id: 73d5174f86f331bd7244af7dc54e03073500f490

--- nn_plot.m
+++ nn_plot.m
@@ -5,10 +5,11 @@
 % fname = '20SDF_H_30_short';
 % fname = 'interp_btw_fr_res';
 
+load(['nn_' fname],'*_ARR','pdt_arr','RSP');
+
+%%
 cmap = {flipud(parula),flipud(parula),jet}; % for "positive" (sequential, sequential)
 % cmap = {flipud(parula),RdYlGn}; % for "both" (sequential, diverging)
-
-load(['nn_' fname]);
 
 [ind_a,ind_b] = find(P_ARR==min(min(P_ARR))); % find best predictors
 % ind_a = 1; ind_b = 4; % Y_0 and mean(d_i)
@@ -27,12 +28,12 @@ for rr = 1:size(y,1)
     
     hold on;
     
-    temp2 = max([max(max(t)),max(max(y))]);
-    temp1 = min([min(min(t)),min(min(y))]);
+    temp2 = max([max(max(RSP)),max(max(y))]);
+    temp1 = min([min(min(RSP)),min(min(y))]);
     axis([temp1,temp2,temp1,temp2]);
     p = plot([temp1,temp2],[temp1,temp2],'color',0.75*[1,1,1],'linewidth',1);
     
-    h = scatter(t(rr,ind),y(rr,ind),20,c_map(rr,:),'filled');
+    h = scatter(RSP(rr,ind),y(rr,ind),20,c_map(rr,:),'filled');
     alpha(h,0.5);
     
     [r,m,b] = regression(t(rr,ind),y(rr,ind));
@@ -60,17 +61,17 @@ for rr = 1:size(y,1)
     figure;
     hold on;
     
-    plt = abs(y(rr,ind) - t(rr,ind));
-    h = scatter(t(rr,ind),plt,10,'k','filled');
+    plt = abs(y(rr,ind) - RSP(rr,ind));
+    h = scatter(RSP(rr,ind),plt,10,'k','filled');
     alpha(h,0.5);
     
     mplt = mean(plt);
-    seplt = std(plt)/sqrt(length(plt));
+    seplt = std(plt);%/sqrt(length(plt));
     %     h1 = plot([min(t(rr,ind)),max(t(rr,ind))],mplt*ones(1,2),'k');
     %     h2 = plot([min(t(rr,ind)),max(t(rr,ind))],(mplt+seplt)*ones(1,2),'--k');
     %     plot([min(t(rr,ind)),max(t(rr,ind))],(mplt-seplt)*ones(1,2),'--k');
     %     legend([h1,h2],num2str(mplt,3),['\pm ' num2str(seplt,3) ' (SE)'],'location','northwest');
-    title(['mean = ' num2str(mplt,3) '\pm ' num2str(seplt,3) ' (SE)'],'fontweight','normal');
+    title(['mean = ' num2str(mplt,3) '\pm ' num2str(seplt,3) ' (STD)'],'fontweight','normal');
     
     xlabel(['actual ' RSP_txt{rr}]);
     ylabel(['|' RSP_txt{rr} ' error|']);
@@ -84,34 +85,34 @@ end
 
 %% plot average error for all predictors
 
-if size(y,1)==2
-    E_avg = mean(E_ARR,2);
-    
-    figure;
-    hold on;
-    
-    % --------- version 1: colormap -----------
-    colormap(hot);
-    scatter(pdt_arr(:,1),pdt_arr(:,2),50,E_avg/size(RSP,2),'filled');
-    cb = colorbar;
-    caxis([0.1,0.8]);
-    ylabel(cb,['norm of all errors / no. of samples']);
-    
-    % --------- version 2: markersize -----------
-    % scatter(pdt_arr(:,1),pdt_arr(:,2),80*E_avg/size(RSP,2),'k','filled');
-    % text(3,3,'marker sizes \propto error');
-    
-    set(gca,'xtick',1:length(PDT_txt),'xticklabel',PDT_txt);
-    set(gca,'ytick',1:length(PDT_txt),'yticklabel',PDT_txt);
-    xlabel('predictor #1');
-    ylabel('predictor #2');
-    xtickangle(30);
-    ytickangle(30);
-    
-    axis equal;
-    set(gca,'fontsize',8);
-    set(gcf,'paperposition',[0,0,4,3]);
-    print('-dtiff','-r300',['nn_' fname '_err_all']);
-    close;
-    
-end
+% % % if size(y,1)==2
+% % %     E_avg = mean(E_ARR,2);
+% % %     
+% % %     figure;
+% % %     hold on;
+% % %     
+% % %     % --------- version 1: colormap -----------
+% % %     colormap(hot);
+% % %     scatter(pdt_arr(:,1),pdt_arr(:,2),50,E_avg/size(RSP,2),'filled');
+% % %     cb = colorbar;
+% % %     caxis([0.1,0.8]);
+% % %     ylabel(cb,['norm of all errors / no. of samples']);
+% % %     
+% % %     % --------- version 2: markersize -----------
+% % %     % scatter(pdt_arr(:,1),pdt_arr(:,2),80*E_avg/size(RSP,2),'k','filled');
+% % %     % text(3,3,'marker sizes \propto error');
+% % %     
+% % %     set(gca,'xtick',1:length(PDT_txt),'xticklabel',PDT_txt);
+% % %     set(gca,'ytick',1:length(PDT_txt),'yticklabel',PDT_txt);
+% % %     xlabel('predictor #1');
+% % %     ylabel('predictor #2');
+% % %     xtickangle(30);
+% % %     ytickangle(30);
+% % %     
+% % %     axis equal;
+% % %     set(gca,'fontsize',8);
+% % %     set(gcf,'paperposition',[0,0,4,3]);
+% % %     print('-dtiff','-r300',['nn_' fname '_err_all']);
+% % %     close;
+% % %     
+% % % end
