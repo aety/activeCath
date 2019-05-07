2134f7f259124d55cd3c829e6dd4df85e4619ef0
aetyang <anneyanget@gmail.com>
2019-05-07 18:28:35 +0200

    minor
    Former-commit-id: 3c60f522ecef49d6780ab253044356b16477778e
    Former-commit-id: 7673334185e7213f38f5ec411a9d70d4eb61d26a
    Former-commit-id: 9bf2feddac590377e2a62eb8d1a95daa690c8a77

--- __simulation/varHelixN/run_varHelixN.m
+++ __simulation/varHelixN/run_varHelixN.m
@@ -1,34 +1,34 @@
 clear; clc; ca;
 n_helix_arr = 4:24;
 
-% for nnn = 1:length(n_helix_arr)
-%     
-%     n_helix = n_helix_arr(nnn);
-%     
-%     d = ['varHelixN_' num2str(n_helix)];
-%     
-%     proc_findApex_3DoF_varHelixN;
-%     clearvars -except nn n_helix_arr n_helix d
-%     
-%     fname = ['findApex_3DoF_varHelixN_' num2str(n_helix)];
-%     load(['proc_' fname]);
-%     
-%     mkdir(d);
-%     cd(d);
-%     pre_nn;
-%     clearvars -except nn n_helix_arr n_helix d fname
-%     
-%     nn_training;
-%     clearvars -except nn n_helix_arr n_helix d fname
-%     
-%     nn_plot;
-%     clearvars -except nn n_helix_arr n_helix d fname
-%     
-%     cd ..
-%     
-% end
+for nnn = 1:length(n_helix_arr)
+    
+    n_helix = n_helix_arr(nnn);
+    
+    d = ['varHelixN_' num2str(n_helix)];
+    
+    proc_findApex_3DoF_varHelixN;
+    clearvars -except nn n_helix_arr n_helix d
+    
+    fname = ['findApex_3DoF_varHelixN_' num2str(n_helix)];
+    load(['proc_' fname]);
+    
+    mkdir(d);
+    cd(d);
+    pre_nn;
+    clearvars -except nn n_helix_arr n_helix d fname
+    
+    nn_training;
+    clearvars -except nn n_helix_arr n_helix d fname
+    
+    nn_plot;
+    clearvars -except nn n_helix_arr n_helix d fname
+    
+    cd ..
+    
+end
 
-% post_nn_varHelixN;
-% clearvars -except nn n_helix_arr n_helix d fname
+post_nn_varHelixN;
+clearvars -except nn n_helix_arr n_helix d fname
 
 plot_pre_nn;
