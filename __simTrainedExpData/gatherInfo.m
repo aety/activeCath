67cf1d04b2467cbc6e86f9642d412d97ee17ce13
aetyang <anneyanget@gmail.com>
2019-05-13 15:36:45 +0200

    deleted old data
    Former-commit-id: 509973188a06672bf238a06c2b2fe21c7821ba13
    Former-commit-id: 9a7424910396a66b3b93b48a52fa3a394eba9cde
    Former-commit-id: c774c1db48a83008d34e3ed78781f16d040616ad

--- __simTrainedExpData/gatherInfo.m
+++
@@ -1,7 +0,0 @@
-load C:\Users\yang\ownCloud\MATLAB\__simulation\varHelixN\varHelixN_16\pre_nn_findApex_3DoF_varHelixN_16 PDT
-load C:\Users\yang\ownCloud\MATLAB\__simulation\varHelixN\varHelixN_16\nn_findApex_3DoF_varHelixN_16 E_ARR pdt_arr
-
-%% retrain to get the best network 
-
-%% load exp data
-load C:\Users\yang\ownCloud\MATLAB\__experiment\roll_bend_pitch\proc\proc_incl_pitch_manualPicking
