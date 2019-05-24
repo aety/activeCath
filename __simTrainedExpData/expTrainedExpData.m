6c5040781b6f4ae12627ef6433fbdc2103906442
aetyang <anneyanget@gmail.com>
2019-05-24 18:23:12 +0200

    added expTrainedExpData_plotPeaks.m
    Former-commit-id: 3cc692dad9801d9a01ec426db21a095a51dc84f9
    Former-commit-id: fd0c46c8933cf1f652a56358fb80c90181cc30ff
    Former-commit-id: ce96c4a4bcd236c64fb513ecb0cf3a41dbef6340

--- __simTrainedExpData/expTrainedExpData.m
+++ __simTrainedExpData/expTrainedExpData.m
@@ -4,9 +4,7 @@ clear; clc; ca;
 fname = 'expTrainedExpData';
 cd C:\Users\yang\ownCloud\MATLAB\__simTrainedExpData
 load('C:\Users\yang\ownCloud\MATLAB\__experiment\roll_bend\pre_nn\positive\_pre_nn_positive_interp\interp_btw_fr_res');
-load C:\Users\yang\ownCloud\MATLAB\__experiment\roll_bend\pre_nn\positive\pre_nn_20SDF_H_30_short TIPx TIPy n_* *_act_arr
-% load C:\Users\yang\ownCloud\MATLAB\__experiment\roll_bend\pre_nn\positive\pre_nn_20SDF_H_30_short TIPx TIPy RSP
-% load C:\Users\yang\ownCloud\MATLAB\__experiment\roll_bend\proc\proc_auto_data_20SDR-H_30_0003 REF
+load C:\Users\yang\ownCloud\MATLAB\__experiment\roll_bend\pre_nn\positive\pre_nn_20SDF_H_30_short TIPx TIPy *_act_arr
 
 %% extract only continuous (roll variation) data
 roll_range = 13:154;
@@ -14,10 +12,12 @@ PKS1 = PKS1(:,:,roll_range,:);
 PKS2 = PKS2(:,:,roll_range,:);
 th_roll_act_arr = th_roll_act_arr(roll_range);
 
-%% modify exp data for compatibility with simulation data
-% ref_pt = unique(REF); ref_pt = -fliplr(ref_pt);                       % flip x-y and signs
+pks_range = 1:14;
+PKS1 = PKS1(pks_range,:,:,:);
+PKS2 = PKS2(pks_range,:,:,:);
 
-n_helix = 16;
+%% modify exp data for compatibility with simulation data
+% n_helix = 16; % for simulation (comparison)
 PKS_scale = cell(1,size(PKS1,3)*size(PKS1,4));
 b_arr = nan(size(PKS1,3)*size(PKS1,4),1); p_arr = b_arr; r_arr = b_arr;
 nn = 0;
@@ -39,16 +39,16 @@ for dd = 1:size(PKS1,4) % bend
         
         disp([num2str(ii) '/' num2str(size(PKS1,3)) ', ' num2str(dd) '/' num2str(size(PKS1,4))]);
         %         [x_pks,y_pks,tgl,~,~,ref_pt_sim] = proc_findApex_3DoF_varHelixN_Func(bend_arr,roll_arr,pitch_arr,n_helix);
-        
+        %
         %         temp3 = x_pks - ref_pt_sim(1); % simulated peaks X
         %         temp4 = y_pks - ref_pt_sim(2); % simulated peaks Y
         
         %% scale peaks (according to a factor determined by zero angles)
         %         fac1 = rssq([range(temp1),range(temp2)]);
         %         fac3 = rssq([range(temp3),range(temp4)]);
-        fac = 0.1965; % fac3/fac1; (based on ii = end, dd = 1)
-        temp1 = temp1*fac;
-        temp2 = temp2*fac;
+        %         fac = 0.1965; % fac3/fac1; % (based on ii = end, dd = 1)
+        %         temp1 = temp1*fac;
+        %         temp2 = temp2*fac;
         
         nn = nn + 1;
         PKS_scale{nn}(1,:) = temp1;
@@ -62,12 +62,13 @@ for dd = 1:size(PKS1,4) % bend
         %         hold on;
         %         h1 = plot(PKS_scale{nn}(1,:),PKS_scale{nn}(2,:),'*k');
         %         plot(PKS_scale{nn}(1,logical(temp0)),PKS_scale{nn}(2,logical(temp0)),'*r');
-        %         h2 = plot(temp3,temp4,'ok');
-        %         plot(temp3(tgl),temp4(tgl),'or');
-        %         legend([h1,h2],'exp','sim','location','northwest');
-        %         title([bend_arr,roll_arr,pitch_arr]);
-        %         axis equal
-        %         pause(0.01);
+        %         text(PKS_scale{nn}(1,1),PKS_scale{nn}(2,1),'1');
+        %         %         h2 = plot(temp3,temp4,'ok');
+        %         %         plot(temp3(tgl),temp4(tgl),'or');
+        %         %         legend([h1,h2],'exp','sim','location','northwest');
+        %         %         title([bend_arr,roll_arr,pitch_arr]);
+        %         %         axis equal
+        %         pause;%(0.01);
         %         clf;
         
         clear temp* fac*
@@ -75,17 +76,14 @@ for dd = 1:size(PKS1,4) % bend
     end
 end
 
-
 %% update peaks and reference point
-ref_pt = [0,0]; % ref_pt_sim;
+ref_pt = [0,0];
 PKS = PKS_scale;
 X = nan(100,nn);
 Y = nan(100,nn);
 
 %% compile for NN and rename
 pre_nn;
-
-%%
-n_pdt = 2;
+n_pdt = 2; 
 nn_training;
 nn_plot;
