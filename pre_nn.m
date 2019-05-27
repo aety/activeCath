f4c80c8db9cd743acb277048e0572cf512529fdd
aetyang <anneyanget@gmail.com>
2019-05-27 14:07:00 +0200

    revised predictor number 5 so it is completely based on peaks, i.e. no longer requires the TIPx and TIPy information
    Former-commit-id: 90f16dd9645deb1d6ba39b0fd35b364226a11335
    Former-commit-id: cdb1e4a20b824142a6ecccee2ee4eb193045db1c
    Former-commit-id: 2c133a9f216aacd20f3e20c041c16d47d8744acf

--- pre_nn.m
+++ pre_nn.m
@@ -5,7 +5,7 @@
 % fname = 'findApex_3DoF';
 % load(['proc_' fname]);
 
-PDT_txt = {'Y_0','mean(d_i)','std(d_i)','mean(d_i)_1 - mean(d_i)_2','\alpha_{max}',...%'\alpha_e - \alpha_0',...
+PDT_txt = {'Y_0','mean(d_i)','std(d_i)','mean(d_i)_1 - mean(d_i)_2','\alpha_{distal}',...%'\alpha_e - \alpha_0',...
     'mean[\Delta\alpha]','std[\Delta\alpha]','CV[d_i]','mean(d_i)_1 / mean(d_i)_2'};
 RSP_txt = {'\theta_{roll}','\theta_{bend}','\theta_{pitch}'};
 
@@ -25,16 +25,16 @@ for nn = 1:n_fr
     PXY(1,:) = PXY(1,:) - ref(1);
     PXY(2,:) = PXY(2,:) - ref(2);
     
-    % separate    
+    % separate
     pxy1 = PXY(:,tgl);
     pxy2 = PXY(:,~tgl);
     if size(pxy1,1)<=2
         pxy1 = pxy1';
         pxy2 = pxy2';
     end
-    plt1 = pxy1; 
-    plt2 = pxy2; 
-        
+    plt1 = pxy1;
+    plt2 = pxy2;
+    
     %% compile NN predictors (set 1 -- on the right) (set 2 -- on the left)
     slc = plt2(1,1) < plt1(1,1);    % pick a point at the lowest x-position
     temp = [plt1(1,1),plt2(1,1)];   % pick a point at the lowest x-position
@@ -43,18 +43,19 @@ for nn = 1:n_fr
     dlat2 = rssq(diff(plt2)')'; % local lateral distance (set 2)
     dlat = [dlat1;dlat2];       % all local distances (both sets)
     
-    alp1 = atan2(diff(plt1(:,2)),diff(plt1(:,1))); % local slope angle (set 1)  
-    alp2 = atan2(diff(plt2(:,2)),diff(plt2(:,1))); % local slope angle (set 2)  
+    alp1 = atan2(diff(plt1(:,2)),diff(plt1(:,1))); % local slope angle (set 1)
+    alp2 = atan2(diff(plt2(:,2)),diff(plt2(:,1))); % local slope angle (set 2)
     dalp1 = diff(alp1);
     dalp2 = diff(alp2);
     dalp = [dalp1;dalp2];
+    
+    xy_avg_dist = mean([plt1(end,:);plt2(end,:)]); % average x-y coordinate of the most distal peaks on two sides
     
     PDT(1,nn) = temp(slc+1);                            % predictor 1 -- d0
     PDT(2,nn) = nanmean(dlat);                          % predictor 2 -- mean(di)
     PDT(3,nn) = nanstd(dlat);                           % predictor 3 -- std(di)
     PDT(4,nn) = nanmean(dlat1) - nanmean(dlat2);        % predictor 4 -- mean(di)_left - mean(di)_right
-%     PDT(5,nn) = atan2(TIPy(nn),TIPx(nn));               % predictor 5 -- alpha_e - alpha_0 (global slope angle, assuming base slope is 0)
-    PDT(5,nn) = nanmax(dlat);                           % predictor 5 -- maximum alpha
+    PDT(5,nn) = atan2(xy_avg_dist(2),xy_avg_dist(1));   % predictor 5 -- \alpha_{distal}
     PDT(6,nn) = nanmean(dalp);                          % predictor 6 -- mean[del(alpha)]
     PDT(7,nn) = nanstd(dalp);                           % predictor 7 -- std[del(alpha)]
     PDT(8,nn) = nanstd(dlat)/nanmean(dlat);             % predictor 8 -- CV[di]
