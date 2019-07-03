98975ca1ca13e514dde059a8ccaad1097b8f1a94
aetyang <anneyanget@gmail.com>
2019-07-03 17:10:48 +0200

    Adjusted CNN parameters-- still not working for raw x-ray images

--- CNN/CNN_test_apply.m
+++ CNN/CNN_test_apply.m
@@ -1,54 +1,11 @@
 clear; ca; clc;
 
-%% Load Data
-filearr = {'03','21','67','83','99'};
-
-nn = 0;
-
-scale_fac = 0.1;
-xr = 1:650*scale_fac;
-yr = 1:550*scale_fac;
-
-imsize = [range(xr)+1,range(yr)+1];
-
-tr_pct = 0.70;
-n_fr = 375; % length(ind_arr);
-trainIdx = 1:n_fr; % randperm(n_fr,round(tr_pct*n_fr));
-validIdx = 1:n_fr; % validIdx(trainIdx) = [];
-
-XTrain = nan(imsize(1),imsize(2),1,length(trainIdx)*length(filearr));
-XValidation = nan(imsize(1),imsize(2),1,length(validIdx)*length(filearr));
-YTrain = [];
-YValidation = [];
-
-bend_arr = 0:20:80;
-
-for aa = 1:length(filearr)
-    load(['C:\Users\yang\ownCloud\MATLAB_largefiles\__experiment\roll_bend\proc\proc_auto_data_20SDR-H_30_00' filearr{aa} '_wImage.mat']);
-    
-    for ff = 1:length(trainIdx)
-        disp([aa,ff]);
-        nn = nn + 1;
-        temp = mat2gray(I_disp_arr{trainIdx(ff)});
-        temp = imresize(temp,0.1);
-        temp = imadjust(temp(xr,yr));
-        XTrain(:,:,1,ff) = temp;
-        
-    end
-    YTrain = [YTrain; th1_arr(ind_arr(trainIdx)),bend_arr(aa)*ones(n_fr,1)];
-    
-    
-    for ff = 1:length(validIdx)
-        temp = mat2gray(I_disp_arr{validIdx(ff)});
-        temp = imresize(temp,0.1);
-        temp = imadjust(temp(xr,yr));
-        XValidation(:,:,1,ff) = temp;
-    end
-    YValidation = [YValidation; th1_arr(ind_arr(validIdx)),bend_arr(aa)*ones(n_fr,1)];
-    
-end
-
 %% Check Data Normalization
+load CNN_test_apply_data
+
+YTrain = normalize(YTrain);
+YValidation = normalize(YValidation);
+
 figure
 histogram(YTrain(:,1))
 figure;
@@ -58,38 +15,70 @@ ylabel('Counts')
 xlabel('Bend Angle')
 
 %% Create Network Layers
-ftsize = 3;
-n_output = 2;
+FilterSize = 3; % odd number
+NumFilters = 24; % number of channels, high numbers are linked to overfitting
+% Stride = 1; % default
+Padding = 'same';
+
+PoolSize = 2; % 
+Stride = 2; % number of pixels to skip (to decrease size)
+
+OutputSize = 2; % number of output variables
 
 layers = [
+    % input 
     imageInputLayer([imsize, 1])
     
-    convolution2dLayer(ftsize,8,'Padding','same')
+    convolution2dLayer(FilterSize,NumFilters,'Padding',Padding) 
     batchNormalizationLayer
     reluLayer
     
-    averagePooling2dLayer(2,'Stride',2)
+    averagePooling2dLayer(PoolSize,'Stride',Stride) % 1 
     
-    convolution2dLayer(ftsize,16,'Padding','same')
+    convolution2dLayer(FilterSize,NumFilters,'Padding',Padding)
     batchNormalizationLayer
     reluLayer
     
-    averagePooling2dLayer(2,'Stride',2)
+    averagePooling2dLayer(PoolSize,'Stride',Stride) % 2
     
-    convolution2dLayer(ftsize,32,'Padding','same')
+    convolution2dLayer(FilterSize,NumFilters,'Padding',Padding)
     batchNormalizationLayer
     reluLayer
     
-    convolution2dLayer(ftsize,32,'Padding','same')
+    averagePooling2dLayer(PoolSize,'Stride',Stride) % 3 
+    
+    convolution2dLayer(FilterSize,NumFilters,'Padding',Padding)
+    batchNormalizationLayer
+    reluLayer
+    
+    averagePooling2dLayer(PoolSize,'Stride',Stride) % 4
+    
+    convolution2dLayer(FilterSize,NumFilters,'Padding',Padding)
+    batchNormalizationLayer
+    reluLayer
+    
+    averagePooling2dLayer(PoolSize,'Stride',Stride) % 5
+    
+    convolution2dLayer(FilterSize,NumFilters,'Padding',Padding)
+    batchNormalizationLayer
+    reluLayer
+    
+    averagePooling2dLayer(PoolSize,'Stride',Stride) % 6
+    
+    convolution2dLayer(FilterSize,NumFilters,'Padding',Padding)
+    batchNormalizationLayer
+    reluLayer
+    
+    convolution2dLayer(FilterSize,NumFilters,'Padding',Padding)
     batchNormalizationLayer
     reluLayer
     
     dropoutLayer(0.2)
-    fullyConnectedLayer(n_output) % number of output variables
+    fullyConnectedLayer(OutputSize) % number of output variables
     regressionLayer];
 
 %% Train Network
-miniBatchSize  = 128;
+miniBatchSize  = 32; % ~1875/40
 validationFrequency = floor(numel(YTrain)/miniBatchSize);
 options = trainingOptions('sgdm', ...
     'MiniBatchSize',miniBatchSize, ...
@@ -123,13 +112,17 @@ squares = predictionError.^2;
 rmse = sqrt(mean(squares));
 
 figure;
-plot(predictionError,'*-');
-
-% residualMatrix = reshape(predictionError,15,25);
-
-% figure
-% boxplot(residualMatrix);%,...
-%     'Labels',{'0','1','2','3','4','5','6','7','8','9'})
-% xlabel('Digit Class')
-% ylabel('Degrees Error')
-% title('Residuals')
+hold on;
+h = scatter(YValidation(:,1),YPredicted(:,1),10,'filled');
+alpha(h,0.5);
+h = scatter(YValidation(:,2),YPredicted(:,2),10,'filled');
+alpha(h,0.5);
+axis equal
+legend('\theta_{bend}','\theta_{roll}','location','northwest');
+xlabel('ground truth (norm.)');
+ylabel('predicted');
+box off
+set(gca,'fontsize',10);
+set(gcf,'paperposition',[0,0,3,3],'unit','inches');
+% print('-dtiff','-r300','CNN_test_apply_peaks_corr');
+% close;
---
+++ CNN/CNN_test_apply_data.m
@@ -0,0 +1,47 @@
+%% Load Data
+filearr = {'03','21','67','83','99'};
+
+nn = 0;
+
+scale_fac = 1;
+xr = 51:520; % 1:650*scale_fac;
+yr = 101:400; % 1:550*scale_fac;
+
+imsize = [range(xr)+1,range(yr)+1];
+
+tr_pct = 0.70;
+n_roll = 375;
+n_bend = 5;
+n_fr = n_roll*n_bend; % length(ind_arr);
+trainIdx = randperm(n_fr,round(tr_pct*n_fr));
+validIdx = 1:n_fr; validIdx(trainIdx) = [];
+
+X = nan(imsize(1),imsize(2),1,n_fr);
+Y = [];
+
+bend_arr = 0:20:80;
+
+for aa = 1:n_bend
+    load(['C:\Users\yang\ownCloud\MATLAB_largefiles\__experiment\roll_bend\proc\proc_auto_data_20SDR-H_30_00' filearr{aa} '_wImage.mat'],'I_disp_arr','th1_arr','ind_arr');
+    
+    for ff = 1:n_roll
+        disp([aa,ff]);
+        nn = nn + 1;
+        temp = mat2gray(I_disp_arr{ff});
+%         temp = imresize(temp,scale_fac);
+        temp = imadjust(temp(xr,yr));
+        temp = imbinarize(imsharpen(temp));
+        X(:,:,1,ff) = temp;
+        
+    end
+    Y = [Y; th1_arr(ind_arr),bend_arr(aa)*ones(n_roll,1)];
+        
+end
+
+XTrain = X(:,:,1,trainIdx); 
+XValidation = X(:,:,1,validIdx);
+
+YTrain = Y(trainIdx,:); 
+YValidation = Y(validIdx,:);
+
+save CNN_test_apply_data XValidation YValidation XTrain YTrain imsize
